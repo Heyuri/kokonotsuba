b47e1cde55f246aedc5734122a885d064a87bc04
RealAngeleno <119266468+RealAngeleno@users.noreply.github.com>
2023-01-07 10:44:24 -0800

    Remove trailing comma which causes a parse error
    Parse error: syntax error, unexpected ')' in [path-to-kokonotsuba-directory]/install.php on line 167

--- install.php
+++ install.php
@@ -163,7 +163,7 @@ if (isset($_POST["submit"])) {
 				$data[4], $data[5], $data[6], $data[7],
 				$data[8], $data[9], $data[10], $data[11],
 				$data[12], $data[13], $data[14], $data[15],
-				$data[16], $com, $data[18], $data[19],
+				$data[16], $com, $data[18], $data[19]
 			);
 			if (!($stmt->execute())) {
 				echo "<br>FAILURE on post #".$data[0]."\n";
