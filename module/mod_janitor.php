0f2c2a65147ab62c4574d910881fdb994bcd7b77
komeo <63191092+k0me0@users.noreply.github.com>
2024-01-12 21:34:14 +0100

    Public warn reason

--- module/mod_janitor.php
+++ module/mod_janitor.php
@@ -34,7 +34,7 @@ class mod_janitor extends ModuleHelper {
 		if ($_SERVER['REQUEST_METHOD']!='POST') { 
 			$dat = '';
 			head($dat);
-			$dat .= '
+			$dat .= '[<a href="'.PHP_SELF2.'?'.$_SERVER['REQUEST_TIME'].'">Return</a>]<br>
 			<fieldset class="menu" style="display: inline-block;"><legend>Warn User</legend>
 				<form action="'.PHP_SELF.'" method="POST">
 					<input type="hidden" name="mode" value="module" />
@@ -42,6 +42,7 @@ class mod_janitor extends ModuleHelper {
 					<label>Post No.<input type="number" name="no" min="0" value="'.($_GET['no']??'0').'" /></label><br />
 					<label>Reason:<br />
 						<textarea name="msg" cols="80" rows="6">No reason given.</textarea></label><br />
+					<label>Public? <input type="checkbox" name="public">
 					<center><input type="submit" value="Warn"></center>
 			</form>
 			</fieldset>
@@ -56,6 +57,13 @@ class mod_janitor extends ModuleHelper {
 			$ip = $post['host'];
 			$reason = str_replace(",", "&#44;", preg_replace("/[\r\n]/", '', nl2br($_POST['msg']??'')));
 			if(!$reason) $reason='No reason given.';
+			if ($_POST["public"]) {
+				$post['com'] .= "<br \><br \><b class=\"warning\">($reason)</b> <img style=\"vertical-align: baseline;\" src=\"https://static.heyuri.net/image/hammer.gif\">";
+				$PIO->updatePost($no, $post);
+				$PIO->dbCommit();
+				$parentNo = $post['resto'] ? $post['resto'] : $post['no'];
+				deleteCache(array($parentNo));
+			}
 			$log = array_map('rtrim', file($this->BANFILE));
 			$f = fopen($this->BANFILE, 'w');
 			$rtime = $_SERVER['REQUEST_TIME'];
@@ -63,6 +71,7 @@ class mod_janitor extends ModuleHelper {
 			logtime('Warned '.$ip, valid());
 			
 			fclose($f);
+			updatelog();
 			redirect('back', 0);
 		}
 	}
