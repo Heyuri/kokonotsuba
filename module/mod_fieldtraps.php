7dc83307e6c67a58bc2853a46406703e3af6debe
komeo <63191092+k0me0@users.noreply.github.com>
2024-01-28 22:52:18 +0100

    Add files via upload

---
+++ module/mod_fieldtraps.php
@@ -0,0 +1,26 @@
+<?php
+// komeo 2024
+
+class mod_fieldtraps extends ModuleHelper {
+	private $fields = array('e-mail', 'username', 'subject', 'comment', 'firstname', 'lastname', 'city', 'state', 'zipcode');
+
+	public function getModuleName(){
+		return __CLASS__.' : Field traps';
+	}
+
+	public function getModuleVersionInfo(){
+		return 'Koko BBS Release 1';
+	}
+
+	public function autoHookPostForm(&$txt){
+		foreach ($this->fields as &$f) {
+			$txt .= '<input maxlength="100" type="text" name="'.$f.'" id="'.$f.'" size="28" value="" class="inputtext" style="display: none;">';
+		}
+	}
+
+	public function autoHookRegistBegin(&$name, &$email, &$sub, &$com, $upfileInfo, $accessInfo){
+		foreach ($this->fields as &$f) {
+			if ($_POST[$f] != "") die;
+		}
+	}
+}
