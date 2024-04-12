d68d744b6a9944c93d0d594e8ca2f9e4bf66d7e3
chen-chan1 <grimmkiler@yahoo.com>
2024-04-12 02:54:48 +0000

    test

---
+++ newBoard.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once __DIR__ .'/classes/board.php';
+require_once __DIR__ .'/classes/repos/repoBoard.php';
+$BOARDREPO = BoardRepoClass::getInstance();
+
+$name =  uniqid();
+
+copy(__DIR__ . "/boardConfigs/baseConf.php", __DIR__ . "/boardConfigs/". $name . ".php");
+
+$board = new boardClass(__DIR__ . "/boardConfigs",0);
+
+$BOARDREPO->createBoard($board);
+
