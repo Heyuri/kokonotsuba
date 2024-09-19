fe33b4fac0c972a6f2092864d13a8dd6d01601ad
kaguy4 <kaguya@kolyma.net>
2024-09-19 10:42:12 +0300

    some improvements

--- static/js/flashembed.js
+++ static/js/flashembed.js
@@ -1,10 +1,9 @@
 //chatgpt-sensei & hachichigo
-
-const staticURL = "https://yuisuki.net/flashtemp/kokonotsuba-portable/static/";
+const staticURL = "./static/";
 
 function openFlashEmbedWindow(file, name, w, h) {
 	if (!document.getElementById("swfWindow")) {
-	
+
 		const darkenoverlay = document.createElement("div");
 		darkenoverlay.id = "darken-embed-screen";
 		document.body.appendChild(darkenoverlay);
@@ -20,15 +19,19 @@ function openFlashEmbedWindow(file, name, w, h) {
 			Winw = Math.round(0.8 * window.innerWidth);
 		}
 
-		swfWindow.style.width = `${Winw}px`;
+		swfWindow.style.width = `${Winw - 26}px`;
 		swfWindow.style.height = `${Winh}px`;
 
+		// Enable resizing for the window and hide scrollbars
+		swfWindow.style.resize = "both";
+		swfWindow.style.overflow = "hidden";  // Hide scrollbars
+
 		swfWindow.innerHTML = `
-			<div id="swf-embed-header">
-			<img src="${staticURL}image/cross2embed.png" id="closeButton" onclick="closeSWFWindow()" style="float: right; cursor: pointer;">
-			<div id="embed-swf-details">  ${name},  ${w}x${h}</div>
+			<div id="swf-embed-header" style="cursor: move;">
+				<img src="${staticURL}image/cross2embed.png?v1" id="closeButton" onclick="closeSWFWindow()" style="float: right; cursor: pointer;">
+				<div id="embed-swf-details">${name}, ${w}x${h} <a href="${file}" download="${name}"><div class="download"></div></a></div>
 			</div>
-			<div id="ruffleContainer"></div>
+			<div id="ruffleContainer" style="width: 100%; height: calc(100% - 20px);"></div>
 		`;
 
 		document.body.appendChild(swfWindow);
@@ -36,14 +39,9 @@ function openFlashEmbedWindow(file, name, w, h) {
 		const container = document.getElementById('ruffleContainer');
 
 		if (container) {
-			container.style.width = `${Winw}px`;
-			container.style.height = `${Winh}px`;
-
+			// Load and resize the flash player inside the container
 			const ruffle = window.RufflePlayer.newest();
 			const rufflePlayer = ruffle.createPlayer();
-
-			rufflePlayer.style.width = `${Winw}px`;
-			rufflePlayer.style.height = `${Winh}px`;
 
 			container.appendChild(rufflePlayer);
 
@@ -52,7 +50,19 @@ function openFlashEmbedWindow(file, name, w, h) {
 			}).catch((err) => {
 				console.error("Failed to load Flash file:", err);
 			});
+
+			// Resize the flash player along with the container
+			const resizeObserver = new ResizeObserver(() => {
+				const newWidth = container.offsetWidth;
+				const newHeight = container.offsetHeight;
+				rufflePlayer.style.width = `${newWidth}px`;
+				rufflePlayer.style.height = `${newHeight}px`;
+			});
+			resizeObserver.observe(container);
 		}
+
+		// Add drag functionality to move the window
+		makeElementDraggable(swfWindow, document.getElementById('swf-embed-header'));
 	}
 }
 
@@ -62,5 +72,35 @@ function closeSWFWindow() {
 	if (swfWindow || embedOverlay) {
 		swfWindow.remove();
 		embedOverlay.remove();
+	}
+}
+
+// Function to make the window draggable
+function makeElementDraggable(element, handle) {
+	let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+
+	handle.onmousedown = dragMouseDown;
+
+	function dragMouseDown(e) {
+		e.preventDefault();
+		pos3 = e.clientX;
+		pos4 = e.clientY;
+		document.onmouseup = closeDragElement;
+		document.onmousemove = elementDrag;
+	}
+
+	function elementDrag(e) {
+		e.preventDefault();
+		pos1 = pos3 - e.clientX;
+		pos2 = pos4 - e.clientY;
+		pos3 = e.clientX;
+		pos4 = e.clientY;
+		element.style.top = (element.offsetTop - pos2) + "px";
+		element.style.left = (element.offsetLeft - pos1) + "px";
+	}
+
+	function closeDragElement() {
+		document.onmouseup = null;
+		document.onmousemove = null;
 	}
 }
