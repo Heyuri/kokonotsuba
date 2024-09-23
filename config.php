c0b2ad307d0f54f6982288f2aeb1a9804950a067
anonymage <anon-san@anon.anon.net>
2024-09-24 09:33:19 +1000

    config directory

--- config.php
+++ config.php
@@ -1,15 +1,2 @@
 <?php
-/*
-* This file is for board-specific configurations. Make sure that values in globalconfig.php are set correctly.
-*/
-
-$config['STORAGE_PATH'] = 'dat/'; // Storage directory, needs to have 777 permissions. Include trailing '/'
-require $config['STORAGE_PATH'].'globalconfig.php';
-
-//Board database info
-$config['DATABASE_DBNAME'] =  'database';
-$config['DATABASE_TABLENAME'] = 'sometable';
-
-$config['TITLE'] = 'Kokonotsuba Board'; // Board Title
-$config['TITLESUB'] = ''; // Board Title
-$config['TITLEIMG'] = ''; // Board Title Image (url)
+require '/srv/location/to/board-configs/board-b.php'; //the location of board-specific config file
---
+++ dat/board-configs/board-b.php
@@ -0,0 +1,15 @@
+<?php
+/*
+* This file is for board-specific configurations. Make sure that values in globalconfig.php are set correctly.
+*/
+
+$config['STORAGE_PATH'] = '/srv/location/to/dat/'; // Storage directory, needs to have 777 permissions. Include trailing '/'
+require $config['STORAGE_PATH'].'globalconfig.php';
+
+//Board database info
+$config['DATABASE_DBNAME'] =  'database';
+$config['DATABASE_TABLENAME'] = 'sometable';
+
+$config['TITLE'] = 'Kokonotsuba Board'; // Board Title
+$config['TITLESUB'] = ''; // Board Title
+$config['TITLEIMG'] = ''; // Board Title Image (url)
