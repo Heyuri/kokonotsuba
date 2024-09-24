e01d963e1937d1946741a322c75a902088e4a928
anonymage <anon-san@anon.anon.net>
2024-09-24 15:38:51 +1000

    update config values

--- dat/board-configs/board-b.php
+++ dat/board-configs/board-b.php
@@ -13,3 +13,8 @@ $config['DATABASE_TABLENAME'] = 'sometable';
 $config['TITLE'] = 'Kokonotsuba Board'; // Board Title
 $config['TITLESUB'] = ''; // Board Title
 $config['TITLEIMG'] = ''; // Board Title Image (url)
+
+$config['IMG_DIR'] = 'src/'; // Image Directory
+$config['THUMB_DIR'] = 'src/'; // Thumbnail Directory
+$config['CDN_DIR'] = ''; // absolute path to the folder for storing imgs & thumbs (excluding IMG_DIR, e.g. /var/www/cdn/heyuri/)
+$config['CDN_URL'] = ''; // img/thumb CDN url (without IMG_DIR directory, e.g. https://h.kncdn.org/b/). Set to blank for locally hosted files
--- dat/globalconfig.php
+++ dat/globalconfig.php
@@ -1,6 +1,6 @@
 <?php
 /*
-* Treat this file as a instance-wide config. Overwrite values in here in the root-config if you want board specific settings. Although some settings are exclusive to root-config
+* Treat this file as a instance-wide config. Overwrite values in here in the board-config file for your board, if you want board specific settings.
 */
 
 
@@ -11,6 +11,32 @@ $config['DATABASE_PASSWORD'] ='password';
 $config['DATABASE_DRIVER'] = 'mysqli';
 $config['DATABASE_HOST'] = '127.0.0.1';
 $config['DATABASE_PORT'] = 3306;
+
+$config['ROOTPATH'] = '/srv/location/of/kokonotsuba/directory/'; //Set this to the directory of the backend-files
+
+$config['ALLOW_UPLOAD_EXT'] = 'GIF|JPG|JPEG|PNG|BMP|SWF|WEBM|MP4'; // Allowed filetypes
+
+// Appearance
+$config['ADDITION_INFO'] = @file_get_contents($config['ROOTPATH'].'addinfo.txt'); // Addinfo
+$config['LIMIT_SENSOR'] = array('ByThreadCountCondition'=>150); // AutoDelete, defaults to 10 pages
+
+
+// Appearance
+$config['TEMPLATE_FILE'] = 'templates/kokoimg.tpl'; // Template File. Set this and the next line to 'kokotxt.tpl' and 'kokotxtreply.tpl' respectively to use Kokonotsuba as a textboard.
+$config['REPLY_TEMPLATE_FILE'] = 'templates/kokoimg.tpl'; // Reply page template file
+$config['MAX_AGE_TIME'] = 0; // How long will thread accept age replies? (hours)
+
+$config['USE_CATEGORY'] = 0; // Enable Categories
+
+
+// Ban Settings
+$config['BAN_CHECK'] = 1; // Comprehensive ban check function
+$config['GLOBAL_BANS'] = $config['STORAGE_PATH'].'globalbans.log';
+
+// Webhooks for post notifications
+$config['DISCORD_WH'] = '';
+$config['IRC_WH'] = '';
+
 
 $config['TIME_ZONE'] = '0'; // Timezones, 0 is UTC. Example: '-4' for New York, or '9' for Japan
 $config['PIXMICAT_LANGUAGE'] = 'en_US'; // Language (available languages in /lib/lang/)
@@ -135,6 +161,42 @@ $config['FORTUNES'] = array( // Used for fortune function, selected at random.
 	'ｷﾀ━━━━━━(ﾟ∀ﾟ)━━━━━━ !!!!',
 	'（　´_ゝ`）ﾌｰﾝ'
 );
+
+// Module List
+$config['ModuleList'] = array(
+	/* modes */
+	'mod_soudane',
+	'mod_rss',
+	'mod_cat',
+	'mod_search',
+	'mod_stat',
+	/* admin */
+	'mod_admindel',
+	'mod_adminban',
+	/* thread modes */
+	'mod_autosage',
+	'mod_stop',
+	'mod_sticky',
+	/* posting */
+	'mod_csrf_prevent',
+	'mod_bbcode',
+	'mod_wf',
+	'mod_anigif',
+);
+
+// Module-specific options
+//mod_countryflags
+$config['ModuleSettings']['FLAG_MODE'] = 1; // For the country flags module: 1 = hide flags on posts with "flag" in email field, 2 = show flags on posts with "flag" in email field
+//mod_admindel
+$config['ModuleSettings']['JANIMUTE_LENGTH'] = 20; // Janitor mute duration (in minutes)
+$config['ModuleSettings']['JANIMUTE_REASON'] = 'You have been muted temporarily!'; // Janitor mute reason
+//mod_antiflood
+$config['ModuleSettings']['RENZOKU3'] = 30; // How many seconds between new threads?
+//mod_showip
+$config['ModuleSettings']['IPTOGGLE'] = 1; // 1 to have OPs toggle IP display, 2 enables for all posts
+
+
+$config['ACCOUNT_FLATFILE'] = $config['STORAGE_PATH'].'accounts.txt';
 
 $config['BAD_STRING'] = array();
 $config['BAD_FILEMD5'] = array();
