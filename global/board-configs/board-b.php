5b891c6ab43b01753fa88cc748804b6a6ac50bb0
hachikuji-san <160812410+hachikuji-san@users.noreply.github.com>
2024-12-25 12:01:45 +0000

    Add files via upload

--- global/board-configs/board-b.php
+++ global/board-configs/board-b.php
@@ -3,25 +3,14 @@
 * This file is for board-specific configurations. Make sure that values in globalconfig.php are set correctly.
 */
 
-$config['GLOBAL_PATH'] = './global/'; //Directory for files used by all boards. Include trailing '/'
 $config['STORAGE_PATH'] = './dat/'; // Board storage directory, needs to have 777 permissions. Include trailing '/'
 
-require $config['GLOBAL_PATH'].'globalconfig.php';
+require __DIR__.'/../globalBoardConfig.php';
 
-
-//Board database info
-$config['DATABASE_DBNAME'] =  'database';
-$config['DATABASE_TABLENAME'] = 'sometable';
-
-$config['TITLE'] = 'Kokonotsuba Board'; // Board Title
-$config['TITLESUB'] = ''; // Board Title
 $config['TITLEIMG'] = ''; // Board Title Image (url)
 
 $config['IMG_DIR'] = 'src/'; // Image Directory
 $config['THUMB_DIR'] = 'src/'; // Thumbnail Directory
-$config['CDN_DIR'] = ''; // absolute path to the folder for storing imgs & thumbs (excluding IMG_DIR, e.g. /var/www/cdn/heyuri/)
-$config['CDN_URL'] = ''; // img/thumb CDN url (without IMG_DIR directory, e.g. https://h.kncdn.org/b/). Set to blank for locally hosted files
-
 
 /* 
 * Board modules - you can adjust module settings specifically for this board by overwriting the value of the module to false or 0
---
+++ global/board-configs/board-template.php
@@ -0,0 +1,11 @@
+<?php
+
+$config['STORAGE_PATH'] = './dat/';
+
+require __DIR__.'/../globalBoardConfig.php';
+
+
+$config['TITLEIMG'] = '';
+
+$config['IMG_DIR'] = 'src/';
+$config['THUMB_DIR'] = 'src/';
