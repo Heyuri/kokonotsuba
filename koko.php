da2eb9358d0abac0c080ae60285e6fdba9a13003
hachikuji <anon-san@anon.anon.net>
2025-04-05 08:33:53 +1100

    rearrange singletons and move rquires to includes

--- koko.php
+++ koko.php
@@ -6,73 +6,6 @@ YOU MUST GIVE CREDIT TO WWW.HEYURI.NET ON YOUR BBS IF YOU ARE PLANNING TO USE TH
 */
 session_start();
 
-/* Libraries */
-require __DIR__.'/lib/interfaces.php';
-require __DIR__.'/lib/lib_simplelogger.php';
-require __DIR__.'/lib/lib_loggerinterceptor.php';
-require __DIR__.'/lib/lib_admin.php'; // Admin panel functions
-require __DIR__.'/lib/lib_template.php'; // Template library
-require __DIR__.'/lib/templateEngine.php'; // Post and thread functions
-require __DIR__.'/lib/lib_post.php';
-require __DIR__.'/lib/lib_pms.php';
-require __DIR__.'/lib/lib_pio.php';
-require __DIR__.'/lib/lib_pio.cond.php';
-require __DIR__.'/lib/lib_common.php'; // Introduce common function archives
-require __DIR__.'/lib/pmclibrary.php'; // Ingest libraries
-require __DIR__.'/lib/lib_errorhandler.php'; // Introduce global error capture
-require __DIR__.'/lib/lib_compatible.php'; // Introduce compatible libraries
-
-
-/* Caching */
-require __DIR__.'/lib/boardPathCachingIO.php';
-require __DIR__.'/lib/cachedBoardPath.php';
-
-/* Database singleton */
-require __DIR__.'/lib/database.php';
-
-/* Handle soft error pages */
-require __DIR__.'/lib/softErrorHandler.php';
-
-/* HTML output */
-require __DIR__.'/lib/globalHTML.php';
-
-/* Main output */
-require __DIR__.'/lib/modeHandler.php';
-
-/* Overboard */
-require __DIR__.'/lib/overboard.php';
-
-/* Post objects and singletons */
-require __DIR__.'/lib/postValidator.php';
-require __DIR__.'/lib/postSingleton.php';
-require __DIR__.'/lib/postRedirectIO.php';
-require __DIR__.'/lib/threadRedirect.php';
-
-/* Admin page selector */
-require __DIR__.'/lib/adminPageHandler.php';
-
-/* Account Related */
-require __DIR__.'/lib/accountIO.php';
-require __DIR__.'/lib/accountClass.php';
-require __DIR__.'/lib/accountRequestHandler.php';
-require __DIR__.'/lib/staffAccountSession.php';
-require __DIR__.'/lib/loginHandler.php';
-require __DIR__.'/lib/authenticate.php';
-
-/* Action log */
-require __DIR__.'/lib/actionClass.php';
-require __DIR__.'/lib/actionLoggerSingleton.php';
-
-
-/* Board classes and singleton */
-require __DIR__.'/lib/boardClass.php';
-require __DIR__.'/lib/boardSingleton.php';
-require __DIR__.'/lib/boardStoredFile.php';
-
-/* IP */
-require __DIR__.'/lib/IPAddress.php';
-require __DIR__.'/lib/IPValidator.php';
-
 function getBackendDir() {
 	return __DIR__.'/';
 }
@@ -93,6 +26,8 @@ function getTemplateConfigArray() {
 	require getBoardConfigDir().'board-template.php';
 	return $config;
 }
+
+require getBackendDir().'includes.php';
 
 function generateNewBoardConfigFile() {
 	$templateConfigPath = getBoardConfigDir().'board-template.php';//config template
@@ -140,26 +75,25 @@ DatabaseConnection::createInstance($dbSettings);
 boardIO::createInstance($dbSettings);
 AccountIO::createInstance($dbSettings);
 ActionLogger::createInstance($dbSettings);
-boardPathCachingIO::createInstance($dbSettings);
+
 postRedirectIO::createInstance($dbSettings);
 
 $board = getBoardFromBootstrapFile();
 
 //board-related singletons
 PMCLibrary::createFileIOInstance($board);
-PMS::createInstance($board);
 PIOPDO::createInstance($dbSettings);
+boardPathCachingIO::createInstance($dbSettings);
 
 try {
 	//handle user requests and gzip compression on request
 	$modeHandler = new modeHandler($board);
 	$modeHandler->handle();
-} catch(Exception $e) {
+} catch(\Throwable $e) {
 	$globalConfig = getGlobalConfig();
 	$globalHTML = new globalHTML($board);
 
 	PMCLibrary::getLoggerInstance($globalConfig['ERROR_HANDLER_FILE'],'Global')->error($e->getMessage());	
 	$globalHTML->error("There has been an error. (;´Д`)");
-	
 }
 clearstatcache();
