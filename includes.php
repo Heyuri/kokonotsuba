d61b308874c225d1f472cbff977b3b70defa3ae8
hachikuji <anon-san@anon.anon.net>
2025-04-16 18:14:22 +1000

    reorganize includes file to fit new code organization

--- includes.php
+++ includes.php
@@ -3,75 +3,101 @@
 * Kokonotsuba! includes/require file.
 */
 
+/* Interfaces */
+require __DIR__.'/code/interfaces/IBoard.php'; // board interface
+require __DIR__.'/code/interfaces/IFileIO.php'; // FileIO inteface
+require __DIR__.'/code/interfaces/ILogger.php'; // Logger interface
+require __DIR__.'/code/interfaces/IModule.php'; // ModuleHelper interface
+require __DIR__.'/code/interfaces/IPIO.php'; // postSingleton interface
+require __DIR__.'/code/interfaces/IPIOCondition.php'; // pio condition interface
+require __DIR__.'/code/interfaces/MethodInterceptor.php'; // method interceptor interface
+
+/* Loggers */
+require __DIR__.'/code/logger/SimpleLogger.php'; // simple logger class
+require __DIR__.'/code/logger/LoggerInterceptor.php'; // logger incterceptor class
+require __DIR__.'/code/logger/NopLogger.php'; // no logging class
+
+/* Language */
+require __DIR__.'/code/LanguageLoader.php';
+
+/* FileIO */
+require __DIR__.'/code/abstract fileio/AbstractFileIO.php';
+require __DIR__.'/code/abstract fileio/AbstractIfsFileIO.php';
+require __DIR__.'/code/fileio/fileio.local.php';
+
+
+/* PMC library (singleton factory) */
+require __DIR__.'/code/pmclibrary.php'; // factory
 
 /* Libraries */
-require __DIR__.'/lib/interfaces.php';
-require __DIR__.'/lib/lib_simplelogger.php';
-require __DIR__.'/lib/lib_loggerinterceptor.php';
-require __DIR__.'/lib/lib_admin.php'; // Admin panel functions
-require __DIR__.'/lib/lib_template.php'; // Template library
-require __DIR__.'/lib/lib_rebuild.php'; // Rebuild library
-require __DIR__.'/lib/templateEngine.php'; // Post and thread functions
-require __DIR__.'/lib/pageRenderer.php'; // Renderer for pages
-require __DIR__.'/lib/lib_post.php';
-require __DIR__.'/lib/lib_pio.php';
-require __DIR__.'/lib/lib_pio.cond.php';
-require __DIR__.'/lib/lib_common.php'; // Introduce common function archives
-require __DIR__.'/lib/pmclibrary.php'; // Ingest libraries
-require __DIR__.'/lib/lib_errorhandler.php'; // Introduce global error capture
-require __DIR__.'/lib/lib_compatible.php'; // Introduce compatible libraries
+require __DIR__.'/code/libraries/lib_admin.php'; // admin panel functions
+require __DIR__.'/code/libraries/lib_template.php'; // template library
+require __DIR__.'/code/libraries/lib_rebuild.php'; // rebuild library
+require __DIR__.'/code/libraries/lib_errorhandler.php'; // error library
+require __DIR__.'/code/libraries/lib_compatible.php'; // compatability library
+require __DIR__.'/code/libraries/lib_common.php'; // general-purpose functions
+require __DIR__.'/code/libraries/lib_post.php'; // post-related functions
+
+/* Sensors */
+require __DIR__.'/code/sensor/PIOSensor.php'; // PIO senssor
+require __DIR__.'/code/sensor/postConditions.php'; // post/thread conditions classes
+
+/* Templating */
+require __DIR__.'/code/template/templateEngine.php'; // post and thread functions
+require __DIR__.'/code/template/pageRenderer.php'; // page renderer
+
+/* html */
+require __DIR__.'/code/html/globalHTML.php'; // html class
 
 /* Module related */
-require __DIR__. '/lib/moduleEngine.php';
-require __DIR__. '/lib/moduleHelper.php';
+require __DIR__. '/code/module classes/moduleEngine.php'; // module manager class
+require __DIR__. '/code/module classes/moduleHelper.php'; // module helper class
 
-/* Caching */
-require __DIR__.'/lib/boardPathCachingIO.php';
-require __DIR__.'/lib/cachedBoardPath.php';
+/* Path caching */
+require __DIR__.'/code/path cache/boardPathCachingIO.php';
+require __DIR__.'/code/path cache/cachedBoardPath.php';
+
 
 /* Database singleton */
-require __DIR__.'/lib/database.php';
+require __DIR__.'/code/database/database.php';
 
 /* Handle soft error pages */
-require __DIR__.'/lib/softErrorHandler.php';
-
-/* HTML output */
-require __DIR__.'/lib/globalHTML.php';
-
-/* Main output */
-require __DIR__.'/lib/modeHandler.php';
+require __DIR__.'/code/error/softErrorHandler.php';
 
 /* Overboard */
-require __DIR__.'/lib/overboard.php';
+require __DIR__.'/code/overboard.php';
 
 /* Post objects and singletons */
-require __DIR__.'/lib/postValidator.php';
-require __DIR__.'/lib/postSingleton.php';
-require __DIR__.'/lib/postRedirectIO.php';
-require __DIR__.'/lib/threadRedirect.php';
+require __DIR__.'/code/post/FlagHelper.php';
+require __DIR__.'/code/post/postValidator.php';
+require __DIR__.'/code/post/postSingleton.php';
+require __DIR__.'/code/post/postRedirectIO.php';
+require __DIR__.'/code/post/threadRedirect.php';
 
-/* Admin page selector */
-require __DIR__.'/lib/adminPageHandler.php';
+/* Routers */
+require __DIR__.'/code/routers/adminPageHandler.php';
+require __DIR__.'/code/routers/modeHandler.php';
 
 /* Account Related */
-require __DIR__.'/lib/accountIO.php';
-require __DIR__.'/lib/accountClass.php';
-require __DIR__.'/lib/accountRequestHandler.php';
-require __DIR__.'/lib/staffAccountSession.php';
-require __DIR__.'/lib/loginHandler.php';
-require __DIR__.'/lib/authenticate.php';
+require __DIR__.'/code/account/accountClass.php';
+require __DIR__.'/code/account/accountIO.php';
+require __DIR__.'/code/account/accountRequestHandler.php';
+require __DIR__.'/code/account/staffAccountSession.php';
+
+/* Log in */
+require __DIR__.'/code/log in/authenticate.php';
+require __DIR__.'/code/log in/loginHandler.php';
 
 /* Action log */
-require __DIR__.'/lib/actionClass.php';
-require __DIR__.'/lib/actionLoggerSingleton.php';
-
+require __DIR__.'/code/action log/actionClass.php';
+require __DIR__.'/code/action log/actionLoggerSingleton.php';
 
 /* Board classes and singleton */
-require __DIR__.'/lib/boardClass.php';
-require __DIR__.'/lib/boardRebuilder.php';
-require __DIR__.'/lib/boardSingleton.php';
-require __DIR__.'/lib/boardStoredFile.php';
+require __DIR__.'/code/board/boardClass.php';
+require __DIR__.'/code/board/boardRebuilder.php';
+require __DIR__.'/code/board/boardSingleton.php';
+require __DIR__.'/code/board/boardStoredFile.php';
 
 /* IP */
-require __DIR__.'/lib/IPAddress.php';
-require __DIR__.'/lib/IPValidator.php';
+require __DIR__.'/code/ip/IPAddress.php';
+require __DIR__.'/code/ip/IPValidator.php';
