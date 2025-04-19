cdb034a7680ee90df5a39b318b7eab2c63266b96
hachikuji <hachikujI@yuisuki.net>
2025-04-19 22:53:23 +1000

    remove bindFilterParamters

--- code/action_log/actionLoggerSingleton.php
+++ code/action_log/actionLoggerSingleton.php
@@ -27,95 +27,11 @@ class actionLogger {
 		return self::$instance;
 	}
 	
-	//set the parameters and handle filters for the filter query
-	private function bindfiltersParameters(&$params, &$query, $filters) {
-		if (isset($filters['board']) && is_array($filters['board'])) {
-			$filters['board'] = array_values(array_filter($filters['board'], function ($value, $key) {
-				return is_numeric($key) && is_numeric($value);
-			}, ARRAY_FILTER_USE_BOTH));
-		}	
-
-		if (isset($filters['board']) && !empty($filters['board'])) {
-			$query .= " AND (";
-			foreach ($filters['board'] as $index => $board) {
-				$query .= ($index > 0 ? " OR " : "") . "board_uid = :board_$index";
-				$params[":board_$index"] = $board;
-			}
-			$query .= ")";
-		}
-
-		if(isset($filters['id'])) {
-			$query .= " AND id = :id";
-			$params[':id'] = intval($filters['id']);
-		}
-		if(isset($filters['time_added'])) {
-			$query .= " AND time_added = :time_added";
-			$params[':time_added'] = $filters['time_added'];
-		}
-		if(!empty($filters['name'])) {
-			$query .= " AND name = :name";
-			$params[':name'] = strval($filters['name']);
-		}
-		if (isset($filters['role']) && !empty($filters['role'])) {
-			$query .= " AND (";
-			foreach ($filters['role'] as $index => $role) {
-				$query .= ($index > 0 ? " OR " : "") . "role = :role_$index";
-				$params[":role_$index"] = intval($role);
-			}
-			$query .= ")";
-		}
-
-
-		if(isset($filters['log_action'])) {
-			$query .= " AND log_action LIKE :log_action";
-			$params[':log_action'] = strval('%'.$filters['log_action'].'%');
-		}
-		
-		//generate part of the filter query for timestamp filtration
-		if(!empty($filters['date_after']) && empty($filters['date_before'])) {
-			$query .= " AND date_added >= :date_after";
-			$params[':date_after'] = $filters['date_after'];
-		}
-		if(!empty($filters['date_before']) && empty($filters['date_after'])) {
-			$query .= " AND date_added <= :date_before";
-			$params[':date_before'] = $filters['date_before'];
-		}
-		if(!empty($filters['date_before']) && !empty($filters['date_after'])) {
-			$query .= " AND date_added BETWEEN :date_before AND :date_after";
-
-
-			$lowerBoundDate = min($filters['date_before'], $filters['date_after']);
-			$upperBoundDate = max($filters['date_before'], $filters['date_after']);
-			$params[':date_before'] = strval($lowerBoundDate);
-			$params[':date_after'] = strval($upperBoundDate);
-		}
-		if(isset($filters['ip_address'])) {
-			//adjust for wildcard
-			$ip_pattern = preg_quote($filters['ip_address'], '/');
-			$ip_pattern = str_replace('\*', '.*', $ip_pattern);
-    		$ip_regex = "^$ip_pattern$";
-
-
-			$query .= " AND ip_address REGEXP :ip_regex";    
-			$params[':ip_regex'] = $ip_regex;
-		}
-		
-		if(isset($filters['deleted'])) {
-			$query .= " AND log_action LIKE :delete";
-			$params[':delete'] = strval('%'."delete".'%');
-		}
-		if(isset($filters['ban'])) {
-			$query .= " AND (log_action LIKE :ban OR log_action LIKE :mute OR log_action LIKE :warn)";
-			$params[':ban'] = strval('%'."ban".'%');
-			$params[':mute'] = strval('%'."mute".'%');
-			$params[':warn'] = strval('%'."warn".'%');
-		}
-	}
 	
 	public function getAmountOfLogEntries($filters = []): int {
 		$query = "SELECT COUNT(*) FROM {$this->tableName} WHERE 1";
 		$params = [];
-		$this->bindfiltersParameters($params, $query, $filters);
+		bindActionLogFiltersParameters($params, $query, $filters);
 		$count = $this->databaseConnection->fetchColumn($query, $params);
 		return $count ?? 0;
 	}
@@ -142,7 +58,7 @@ class actionLogger {
 		$params = [];
 		
 		// Apply filters
-		$this->bindfiltersParameters($params, $query, $filters);
+		bindActionLogFiltersParameters($params, $query, $filters);
 		
 		// Add ORDER BY clause
 		$query .= " ORDER BY {$order} DESC";
