a397ad29143e9a681fbb5699525f269aad10fc35
hachikuji <hachikujI@yuisuki.net>
2025-04-21 14:20:44 +1000

    rearrange tripcode processor

--- code/post/helper/tripcodeProcessor.php
+++ code/post/helper/tripcodeProcessor.php
@@ -3,48 +3,84 @@
 class tripcodeProcessor {
 	private readonly array $config;
 	private readonly globalHTML $globalHTML;
-	private readonly staffAccountFromSession $staffSession;
 
-	public function __construct(array $config, globalHTML $globalHTML, staffAccountFromSession $staffSession) {
+	public function __construct(array $config, globalHTML $globalHTML) {
 		$this->config = $config;
 		$this->globalHTML = $globalHTML;
-		$this->staffSession = $staffSession;
 	}
 
-	public function apply(string &$name, string &$email, string &$dest): void {
-		if (preg_match('/[◆◇♢♦⟡★]/u', $name)) {
+	public function apply(string &$name, int $roleLevel = 0): void {
+		if ($this->containsFraudSymbol($name)) {
 			$name .= " (fraudster)";
 		}
 
-		$name = str_replace('&#', '&&', $name);
-		list($name, $trip, $sectrip) = str_replace('&%', '&#', explode('#', $name . '##'));
-		$name = str_replace('&&', '&#', $name);
+		$name = $this->sanitizeNameBeforeTripParsing($name);
+		[$name, $trip, $sectrip] = $this->extractTripParts($name);
+		$name = $this->sanitizeNameAfterTripParsing($name);
 
-		$roleLevel = $this->staffSession->getRoleLevel();
-
-		if ($roleLevel >= $this->config['roles']['LEV_JANITOR'] && $sectrip) {
-			$roleMap = [
-				'LEV_JANITOR'		=> 'JCAPCODE_FMT',
-				'LEV_MODERATOR'	=> 'MCAPCODE_FMT',
-				'LEV_ADMIN'			=> 'ACAPCODE_FMT',
-			];
-
-			foreach ($roleMap as $key => $format) {
-				if ($roleLevel >= $this->config['roles'][$key] && $sectrip === ' ' . $this->globalHTML->roleNumberToRoleName($this->config['roles'][$key])) {
-					if (!empty($this->config[$format])) {
-						$name = sprintf($this->config[$format], $name);
-						$name = "<span class=\"postername\">$name</span>";
-						return;
-					}
-				}
+		if ($this->shouldApplyRoleCapcode($roleLevel, $sectrip)) {
+			if ($this->applyRoleCapcode($name, $roleLevel, $sectrip)) {
+				$name = "<span class=\"postername\">$name</span>";
+				return;
 			}
 		}
 
+		$trip = $this->generateTripcode($trip, $sectrip);
+
+		$this->ensureNameSet($name);
+		$name = "<span class=\"postername\">$name</span><span class=\"postertrip\">$trip</span>";
+
+		$name = $this->applyCapcodeOverrides($name, $trip);
+
+	}
+
+	private function containsFraudSymbol(string $name): bool {
+		return preg_match('/[◆◇♢♦⟡★]/u', $name);
+	}
+
+	private function sanitizeNameBeforeTripParsing(string $name): string {
+		return str_replace('&#', '&&', $name);
+	}
+
+	private function extractTripParts(string $name): array {
+		return str_replace('&%', '&#', explode('#', $name . '##'));
+	}
+
+	private function sanitizeNameAfterTripParsing(string $name): string {
+		return str_replace('&&', '&#', $name);
+	}
+
+	private function shouldApplyRoleCapcode(int $roleLevel, ?string $sectrip): bool {
+		return $roleLevel >= ($this->config['roles']['LEV_JANITOR'] ?? 0) && !empty($sectrip);
+	}
+
+	private function applyRoleCapcode(string &$name, int $roleLevel, string $sectrip): bool {
+		$roleMap = [
+			'LEV_JANITOR'		=> 'JCAPCODE_FMT',
+			'LEV_MODERATOR'	=> 'MCAPCODE_FMT',
+			'LEV_ADMIN'			=> 'ACAPCODE_FMT',
+			'LEV_SYSTEM'		=> 'SCAPCODE_FMT',
+		];
+
+		foreach ($roleMap as $key => $format) {
+			if (
+				$roleLevel >= $this->config['roles'][$key] &&
+				$sectrip === ' ' . $this->globalHTML->roleNumberToRoleName($this->config['roles'][$key]) &&
+				!empty($this->config[$format])
+			) {
+				$name = sprintf($this->config[$format], $name);
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	private function generateTripcode(?string $trip, ?string $sectrip): string {
 		if ($trip) {
 			$trip = mb_convert_encoding($trip, 'Shift_JIS', 'UTF-8');
 			$salt = strtr(preg_replace('/[^\.-z]/', '.', substr($trip . 'H.', 1, 2)), ':;<=>?@[\\]^_`', 'ABCDEFGabcdef');
-			$tripcodeCrypt = substr(crypt($trip, $salt), -10);
-			$trip = "◆$tripcodeCrypt";
+			$trip = "◆" . substr(crypt($trip, $salt), -10);
 		}
 
 		if ($sectrip) {
@@ -52,25 +88,25 @@ class tripcodeProcessor {
 			$trip = "★" . substr($sha, 0, 10);
 		}
 
+		return $trip;
+	}
+
+	private function ensureNameSet(string &$name): void {
 		if (!$name || preg_match("/^[ |　|]*$/", $name)) {
 			if ($this->config['ALLOW_NONAME']) {
 				$name = $this->config['DEFAULT_NONAME'];
 			} else {
-				$this->globalHTML->error(_T('regist_withoutname'), $dest);
+				$this->globalHTML->error(_T('regist_withoutname'));
 			}
 		}
+	}
 
-		$name = "<span class=\"postername\">$name</span><span class=\"postertrip\">$trip</span>";
-
+	private function applyCapcodeOverrides(string $name, string $trip): string {
 		if (isset($this->config['CAPCODES'][$trip])) {
 			$cap = $this->config['CAPCODES'][$trip];
-			$name = '<span class="capcodeSection" style="color:' . $cap['color'] . ';">' . $name . '<span class="postercap">' . $cap['cap'] . '</span></span>';
+			return '<span class="capcodeSection" style="color:' . $cap['color'] . ';">' . $name . '<span class="postercap">' . $cap['cap'] . '</span></span>';
 		}
-
-		if (stristr($email, 'vipcode') && defined('VIPDEF')) {
-			$name .= ' <img src="' . $this->config['STATIC_URL'] . 'vip.gif" title="This user is a VIP user" style="vertical-align: middle;margin-top: -2px;" alt="VIP">';
-		}
-
-		$email = preg_replace('/^vipcode$/i', '', $email);
+		return $name;
 	}
+
 }
