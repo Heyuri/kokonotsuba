827c0dce11fa940a0caeae01467aca15437368b8
hachikuji <hachikujI@yuisuki.net>
2025-04-25 00:32:40 +1000

    expand thumbnail object

--- code/file/thumbnail.php
+++ code/file/thumbnail.php
@@ -1,14 +1,17 @@
 <?php
 
 class thumbnail {
-	private readonly int $thumbnailWidth;
-	private readonly int $thumbnailHeight;
+	private int $thumbnailWidth;
+	private int $thumbnailHeight;
+	private string $thumbnailFileName;
 
-	public function __construct(int $thumbnailWidth = 0, int $thumbnailHeight = 0) {
+	public function __construct(int $thumbnailWidth = 0, int $thumbnailHeight = 0, string $thumbnailFileName = '') {
 		$this->thumbnailWidth = $thumbnailWidth;
 		$this->thumbnailHeight = $thumbnailHeight;
+		$this->thumbnailFileName = $thumbnailFileName;
 	}
 
+	// GET
 	public function getThumbnailWidth(): int {
 		return $this->thumbnailWidth ?? 0;
 	}
@@ -16,4 +19,22 @@ class thumbnail {
 	public function getThumbnailHeight(): int {
 		return $this->thumbnailHeight ?? 0;
 	}
+
+	public function getThumbnailFileName(): string {
+		return $this->thumbnailFileName ?? '';
+	}
+
+	// set
+	public function setThumbnailWidth(int $thumbnailWidth): void {
+		$this->thumbnailWidth = $thumbnailWidth;
+	}
+
+	public function setThumbnailHeight(int $thumbnailHeight): void {
+		$this->thumbnailHeight = $thumbnailHeight;
+	}
+
+	public function setThumbnailFileName(string $thumbnailFileName): void {
+		$this->thumbnailFileName = $thumbnailFileName;
+	}
+
 }
