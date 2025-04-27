aed49fb1766c30d6dfa3d374a6d80ef5c3840b74
hachikuji <hachikujI@yuisuki.net>
2025-04-27 20:30:02 +1000

    include import board in route

--- code/routers/routes/boardsRoute.php
+++ code/routers/routes/boardsRoute.php
@@ -39,16 +39,33 @@ class boardsRoute {
 
 		$this->softErrorHandler->handleAuthError($this->config['roles']['LEV_ADMIN']);
 
+		// draw board tablke
 		$boardTableList = $this->globalHTML->drawBoardTable();
-		$template_values = [
+		
+		// set template values
+		$templateValues = [
 			'{$BOARD_LIST}' => $boardTableList,
-			'{$CREATE_BOARD}' => ($authRoleLevel == $this->config['roles']['LEV_ADMIN']) ? $this->adminTemplateEngine->ParseBlock('CREATE_BOARD', [
+			'{$CREATE_BOARD}' =>  '',
+			'{$IMPORT_BOARD}' =>  ''];
+
+		// admin forms for importing/creating boards
+		if($authRoleLevel == $this->config['roles']['LEV_ADMIN']) {
+			$defaultPath = dirname(getcwd()) . '/';
+			// create board form
+			$templateValues['{$CREATE_BOARD}'] = $this->adminTemplateEngine->ParseBlock('CREATE_BOARD', [
 				'{$DEFAULT_CDN_DIR}' => $this->config['CDN_DIR'],
 				'{$DEFAULT_CDN_URL}' => $this->config['CDN_URL'],
 				'{$DEFAULT_ROOT_URL}' => $this->board->getBoardRootURL(),
-				'{$DEFAULT_PATH}' => dirname(getcwd()) . '/'
-			]) : '',
-		];
+				'{$DEFAULT_PATH}' => $defaultPath
+			]);
+
+			// import board form
+			$templateValues['{$IMPORT_BOARD}'] = $this->adminTemplateEngine->ParseBlock('IMPORT_BOARD', [
+				'{$DEFAULT_ROOT_URL}' => $this->board->getBoardRootURL(),
+				'{$DEFAULT_PATH}' => $defaultPath
+			]);
+
+		}
 
 		if (isset($_GET['view'])) {
 			$id = $_GET['view'] ?? null;
@@ -68,29 +85,29 @@ class boardsRoute {
 			$boardStorageDirectoryName = $board->getBoardStorageDirName() ?? '';
 			$boardDate = $board->getDateAdded() ?? '';
 
-			$template_values['{$BOARD_UID}'] = $boardUID;
-			$template_values['{$BOARD_IDENTIFIER}'] = $boardIdentifier;
-			$template_values['{$BOARD_TITLE}'] = $boardTitle;
-			$template_values['{$BOARD_SUB_TITLE}'] = $boardSubtitle;
-			$template_values['{$BOARD_URL}'] = $boardURL;
-			$template_values['{$BOARD_IS_LISTED}'] = $boardListed ? 'True' : 'False';
-			$template_values['{$BOARD_DATE_ADDED}'] = $boardDate;
-			$template_values['{$BOARD_CONFIG_FILE}'] = $boardConfig;
-			$template_values['{$CHECKED}'] = $boardListed ? 'checked' : '';
-			$template_values['{$BOARD_STORAGE_DIR}'] = $boardStorageDirectoryName;
-			$template_values['{$EDIT_BOARD_HTML}'] = $this->adminTemplateEngine->ParseBlock('EDIT_BOARD', $template_values);
+			$templateValues['{$BOARD_UID}'] = $boardUID;
+			$templateValues['{$BOARD_IDENTIFIER}'] = $boardIdentifier;
+			$templateValues['{$BOARD_TITLE}'] = $boardTitle;
+			$templateValues['{$BOARD_SUB_TITLE}'] = $boardSubtitle;
+			$templateValues['{$BOARD_URL}'] = $boardURL;
+			$templateValues['{$BOARD_IS_LISTED}'] = $boardListed ? 'True' : 'False';
+			$templateValues['{$BOARD_DATE_ADDED}'] = $boardDate;
+			$templateValues['{$BOARD_CONFIG_FILE}'] = $boardConfig;
+			$templateValues['{$CHECKED}'] = $boardListed ? 'checked' : '';
+			$templateValues['{$BOARD_STORAGE_DIR}'] = $boardStorageDirectoryName;
+			$templateValues['{$EDIT_BOARD_HTML}'] = $this->adminTemplateEngine->ParseBlock('EDIT_BOARD', $templateValues);
 			
 			// prevent showing editing a reserved board
 			if(!$boardUID === GLOBAL_BOARD_UID) {
-				$template_values['{$EDIT_BOARD_HTML}'] = "<p>This board cannot be edited.</p>"; 
+				$templateValues['{$EDIT_BOARD_HTML}'] = "<p>This board cannot be edited.</p>"; 
 			}
 
-			$viewBoardHtml = $this->adminPageRenderer->ParseBlock('VIEW_BOARD', $template_values);
+			$viewBoardHtml = $this->adminPageRenderer->ParseBlock('VIEW_BOARD', $templateValues);
 			echo $this->adminPageRenderer->ParsePage('GLOBAL_ADMIN_PAGE_CONTENT', ['{$PAGE_CONTENT}' => $viewBoardHtml], true);
 			return;
 		}
 
-		$boardPageHtml = $this->adminTemplateEngine->ParseBlock('BOARD_PAGE', $template_values);
+		$boardPageHtml = $this->adminTemplateEngine->ParseBlock('BOARD_PAGE', $templateValues);
 		echo $this->adminPageRenderer->ParsePage('GLOBAL_ADMIN_PAGE_CONTENT', ['{$PAGE_CONTENT}' => $boardPageHtml], true);
 	}
 }
