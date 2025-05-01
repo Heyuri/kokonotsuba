467d42991be09ad39facc796f9ac066fddda5dff
hachikuji <hachikujI@yuisuki.net>
2025-05-01 12:03:23 +1000

    update includes

--- includes.php
+++ includes.php
@@ -53,7 +53,8 @@ require __DIR__.'/code/template/pageRenderer.php'; // page renderer
 
 /* html */
 require __DIR__.'/code/html/globalHTML.php'; // html class
-require __DIR__.'/code/html/threadRenderer.php'; // html library
+require __DIR__.'/code/html/threadRenderer.php'; // thread rendering class
+require __DIR__.'/code/html/postHtmlFunctions.php'; // post html library
 
 /* Handle soft error pages */
 require __DIR__.'/code/error/softErrorHandler.php';
