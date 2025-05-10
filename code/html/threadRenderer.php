70ab84fa5d720e8e502dcae5f078c983a02c0893
Anonymous <>
2025-05-11 00:08:54 +0100

    Remove deprecated attributes from 404 image thumbs

--- code/html/threadRenderer.php
+++ code/html/threadRenderer.php
@@ -221,7 +221,7 @@ class threadRenderer {
 			} elseif ($ext === ".swf") {
 				$imgsrc = '<a href="'.$imageURL.'" target="_blank" rel="nofollow"><img src="'.$this->config['SWF_THUMB'].'" class="postimg" alt="SWF Embed"></a>';
 			} else {
-				$imgsrc = '<a href="'.$imageURL.'" target="_blank" rel="nofollow"><img src="'.$this->config['STATIC_URL'].'image/nothumb.gif" class="postimg" alt="'.$imgsize.'" hspace="20" vspace="3" border="0" align="left"></a>'; // Default display style 	(when no preview image)
+				$imgsrc = '<a href="'.$imageURL.'" target="_blank" rel="nofollow"><img src="'.$this->config['STATIC_URL'].'image/nothumb.gif" class="postimg" alt="'.$imgsize.'"></a>'; // Default display style 	(when no preview image)
 			}
 		}
 	
