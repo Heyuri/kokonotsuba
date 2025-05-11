017be1d8fc3a2dffe2b14db4ea26ccb9f4f04711
hachikuji <hachikujI@yuisuki.net>
2025-05-12 07:09:47 +1000

    (WIP) mod_search overhaul

--- module/mod_search.php
+++ module/mod_search.php
@@ -1,11 +1,9 @@
 <?php
 class mod_search extends moduleHelper {
 	private $mypage;
-	private $THUMB_EXT = -1;
 
 	public function __construct(moduleEngine $moduleEngine, boardIO $boardIO, pageRenderer $pageRenderer, pageRenderer $adminPageRenderer) {
 		parent::__construct($moduleEngine, $boardIO, $pageRenderer, $adminPageRenderer);		
-		$this->THUMB_EXT = $this->config['THUMB_SETTING']['Format'];
 		$this->mypage = $this->getModulePageURL();
 	}
 
@@ -23,46 +21,47 @@ class mod_search extends moduleHelper {
 
 	public function ModulePage(){
 		$PIO = PIOPDO::getInstance();
-		$threadSingleton = threadSingleton::getinstance();
-
-		$staffSession = new staffAccountFromSession;
 		$globalHTML = new globalHTML($this->board);
 
-		$roleLevel = $staffSession->getRoleLevel();
-		
-		$searchKeyword = isset($_POST['keyword']) ? trim($_POST['keyword']) : ''; // The text you want to search
+		$adminMode = isActiveStaffSession();
+
+		$searchKeyword = isset($_GET['keyword']) ? trim($_GET['keyword']) : ''; // The text you want to search
 		$dat = '';
 		$globalHTML->head($dat);
 		$links = '[<a href="'.$this->config['PHP_SELF2'].'?'.time().'">'._T('return').'</a>]';
 		$dat .= $links;
-		echo $dat;
 		
-		echo '
+		$dat .= '
 			<h2 class="theading2">'._T('search_top').'</h2>
 				<div class="modulePageContent">
 			';
 
-		if($searchKeyword==''){
-			echo '
-				<form action="'.$this->mypage.'" method="post">
+			$keyword = $_GET['keyword'] ?? '';
+			$field = $_GET['field'] ?? 'com';
+			$method = $_GET['method'] ?? 'AND';
+			
+			$dat .= '
+				<form action="'.$this->config['PHP_SELF'].'" method="get">
+					<input type="hidden" name="mode" value="module">
+					<input type="hidden" name="load" value="mod_search">
+			
 					<div id="search">
 						<ul>'._T('search_notice').'</ul>
-						<input type="hidden" name="mode" value="search">
-						
+							
 						<table>
 							<tbody>
 								<tr>
 									<td class="postblock"><label for="searchKeyword">'._T('search_keyword').'</label></td>
-									<td><input type="text" class="inputtext" id="searchKeyword" name="keyword"></td>
+									<td><input type="text" class="inputtext" id="searchKeyword" name="keyword" value="'.htmlspecialchars($keyword, ENT_QUOTES).'"></td>
 								</tr>
 								<tr>
 									<td class="postblock"><label for="searchTarget">'._T('search_target').'</label></td>
 									<td>
 										<select id="searchTarget" name="field">
-											<option value="com" selected="selected">'._T('search_target_comment').'</option>
-											<option value="name">'._T('search_target_name').'</option>
-											<option value="sub">'._T('search_target_topic').'</option>
-											<option value="no">'._T('search_target_number').'</option>
+											<option value="com"'.($field === 'com' ? ' selected="selected"' : '').'>'._T('search_target_comment').'</option>
+											<option value="name"'.($field === 'name' ? ' selected="selected"' : '').'>'._T('search_target_name').'</option>
+											<option value="sub"'.($field === 'sub' ? ' selected="selected"' : '').'>'._T('search_target_topic').'</option>
+											<option value="no"'.($field === 'no' ? ' selected="selected"' : '').'>'._T('search_target_number').'</option>
 										</select>
 									</td>
 								</tr>
@@ -70,57 +69,77 @@ class mod_search extends moduleHelper {
 									<td class="postblock"><label for="searchMethod">'._T('search_method').'</label></td>
 									<td>
 										<select id="searchMethod" name="method">
-											<option value="AND" selected="selected">'._T('search_method_and').'</option>
-											<option value="OR">'._T('search_method_or').'</option>
+											<option value="AND"'.($method === 'AND' ? ' selected="selected"' : '').'>'._T('search_method_and').'</option>
+											<option value="OR"'.($method === 'OR' ? ' selected="selected"' : '').'>'._T('search_method_or').'</option>
 										</select>
 									</td>
 								</tr>
 							</tbody>
 						</table>
-
+			
 						<div class="buttonSection">
 							<input type="submit" value="'._T('search_submit_btn').'">
 						</div>
 					</div>
 				</form>
 				<hr>
-				';
-		}else{
-			$searchField = $_POST['field']; // Search target (no:number, name:name, sub:title, com:text)
-			$searchMethod = $_POST['method']; // Search method
+			';
+			
+
+		if($searchKeyword){
+			$searchPage = $_GET['page'] ?? 0;
+			$searchPostsPerPage = $this->config['SEARCH_POSTS_PER_PAGE'];
+			$searchPostOffset = $searchPostsPerPage * $searchPage;
+			$quoteLinksFromBoard = getQuoteLinksFromBoard($this->board);
+
+			$searchField = $_GET['field']; // Search target (no:number, name:name, sub:title, com:text)
+			$searchMethod = $_GET['method']; // Search method
 			$searchKeyword = preg_split('/(　| )+/', strtolower(trim($searchKeyword))); // Search text is cut with spaces
 			if ($searchMethod=='REG') $searchMethod = 'AND';
-			$hitPosts = $PIO->searchPost($this->board, $searchKeyword, $searchField, $searchMethod) ?? []; // Directly return the matching article content array
+			$hitPosts = $PIO->searchPosts($this->board, $searchKeyword, $searchField, $searchMethod, $searchPostsPerPage, $searchPostOffset) ?? []; // Directly return the matching article content array
 
-			echo '<div id="searchresult">';
-			$resultlist = '';
-			foreach($hitPosts as $post){
-				extract($post);
-				$resno = $threadSingleton->resolveThreadNumberFromUID($thread_uid);
-				if($this->config['USE_CATEGORY']){
-					$ary_category = explode(',', str_replace('&#44;', ',', $category)); $ary_category = array_map('trim', $ary_category);
-					$ary_category_count = count($ary_category);
-					$ary_category2 = array();
-					for($p = 0; $p < $ary_category_count; $p++){
-						if($c = $ary_category[$p]) $ary_category2[] = '<a href="'.$this->config['PHP_SELF'].'??mode=module&load=mod_searchcategory&&c='.urlencode($c).'">'.$c.'</a>';
-					}
-					$category = implode(', ', $ary_category2);
-				}else $category = '';
-					$com = $globalHTML->quote_link($this->board, $PIO, $com);
-					$com = $globalHTML->quote_unkfunc($com);
-				
-					$arrLabels = array('{$NO}'=>'<a href="'.$this->config['PHP_SELF'].'?res='.($resno?$resno.'#p'.$no:$no).'">'.$no.'</a>', '{$SUB}'=>$sub, '{$NAME}'=>$name, '{$NOW}'=>$now, '{$COM}'=>$com, '{$CATEGORY}'=>$category, '{$NAME_TEXT}'=>_T('post_name'), '{$CATEGORY_TEXT}'=>_T('post_category'));
-				$resultlist .= $this->templateEngine->ParseBlock('SEARCHRESULT',$arrLabels);
+			$resultList = '';
+
+			$templateValues = ['{$BOARD_THREAD_NAME}' => ''];
+
+			$postRenderer = new postRenderer($this->board, $this->config, $globalHTML, $this->moduleEngine, $this->templateEngine, $quoteLinksFromBoard);
+			
+			foreach($hitPosts as $hitPost) {
+				$hitPostThread = $hitPost['thread'];
+				$hitPostData = $hitPost['post'];
+
+				$hitThreadResno = $hitPostThread['post_op_number'];
+				//$hitThreadPosts = $hitPostThread['posts'];
+
+				$postFormExtra = '';
+				$warnBeKill = '';
+				$warnOld = '';
+				$warnHidePost = '';
+				$warnEndReply = '';
+
+				// render the post
+				$resultList .= $postRenderer->render($hitPostData,
+				 $templateValues,
+				 $hitThreadResno,
+				 false,
+				 [$hitPostData],
+				 $adminMode,
+				 $postFormExtra,
+				 $warnBeKill,
+				 $warnOld,
+				 $warnHidePost,
+				 $warnEndReply,
+				 '',
+				 0,
+				 true);
+				$resultList .= $this->templateEngine->ParseBlock('THREADSEPARATE', []); 
 			}
-			echo $resultlist ? $resultlist : '
-				<div class="centerText">
-					<p>'._T('search_notfound').'</p>
-					<p>[<a href="'.$this->mypage.'">'._T('search_back').'</a>]</p>
-				</div>
-				<hr>';
-			echo "</div>";
+
+			$dat .= '<div id="searchresult">';
+			$dat .= $resultList;
+			$dat .= '</div>';
 		}
 
-		echo '</div>';
+		echo $dat;
 	}
 }
