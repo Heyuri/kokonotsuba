fa329eced76b1eeb65ffac19bf9cf883b332342d
hachikuji <hachikujI@yuisuki.net>
2025-05-12 06:23:03 +1000

    expand enums and add methods

--- constants.php
+++ constants.php
@@ -15,11 +15,56 @@ const GLOBAL_BOARD_UID = -1; // number that corrosponds to all boards
 
 
 /* Enums */
-// to be implemented
-enum userRole: string {
-    case LEV_ADMIN = 4;
-    case LEV_MOD = 3;
-    case LEV_JANITOR = 2;
-    case LEV_USER = 1;
-    case LEV_NONE = 0;
+
+// account role
+enum userRole: int {
+	case LEV_NONE = 0;
+	case LEV_USER = 1;
+	case LEV_JANITOR = 2;
+	case LEV_MODERATOR = 3;
+	case LEV_ADMIN = 4;
+	case LEV_SYSTEM = 5;
+
+	/**
+	 * Check if this role is at least as high as another
+	 */
+	public function isAtLeast(self $other): bool {
+		return $this->value >= $other->value;
+	}
+
+	/**
+	 * Check if this role is at most as high as another
+	 */
+	public function isAtMost(self $other): bool {
+		return $this->value <= $other->value;
+	}
+
+	/**
+	 * Check if this role is less than another role
+	 */
+	public function isLessThan(self $other): bool {
+		return $this->value < $other->value;
+	}
+
+	/**
+	 * Check if this is a staff role (Janitor or higher)
+	 */
+	public function isStaff(): bool {
+		return $this->value >= self::LEV_JANITOR->value;
+	}
+
+	/**
+	 * Get a human-readable display name
+	 */
+	public function displayRoleName(): string {
+		return match ($this) {
+			self::LEV_SYSTEM => 'System',
+			self::LEV_ADMIN => 'Admin',
+			self::LEV_MODERATOR => 'Moderator',
+			self::LEV_JANITOR => 'Janitor',
+			self::LEV_USER => 'User',
+			self::LEV_NONE => 'None',
+		};
+	}
+
 }
