6f15bc6b5929d34624b0bb5c4132c46cd3a0765c
hachikuji <hachikujI@yuisuki.net>
2025-05-12 06:28:07 +1000

    update account class

--- code/account/accountClass.php
+++ code/account/accountClass.php
@@ -3,10 +3,27 @@
 class staffAccount {
 	public $username, $role, $password_hash, $id, $number_of_actions, $date_added, $last_login;
 	
-	public function getUsername() { return $this->username; }
-	public function getRoleLevel() { return $this->role; }
-	public function getPasswordHash() { return $this->password_hash; }
-	public function getId() { return $this->id; }
-	public function getLastLogin() { return $this->last_login; }
-	public function getNumberOfActions() { return $this->number_of_actions; }
+	public function getUsername(): string { 
+		return $this->username; 
+	}
+	
+	public function getRoleLevel(): \Kokonotsuba\Root\Constants\userRole { 
+		return \Kokonotsuba\Root\Constants\userRole::tryFrom($this->role); 
+	}
+	
+	public function getPasswordHash() { 
+		return $this->password_hash; 
+	}
+	
+	public function getId() { 
+		return $this->id; 
+	}
+	
+	public function getLastLogin() { 
+		return $this->last_login; 
+	}
+
+	public function getNumberOfActions() {
+		return $this->number_of_actions;
+	}
 }
