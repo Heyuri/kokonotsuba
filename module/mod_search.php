537e4f0a3741fd6a6d71ed01e00b6407537e2e57
hachikuji <hachikujI@yuisuki.net>
2025-05-14 21:40:13 +1000

    implement pager

--- module/mod_search.php
+++ module/mod_search.php
@@ -19,128 +19,131 @@ class mod_search extends moduleHelper {
 		$linkbar .= ' [<a href="'.$this->mypage.'">'._T('head_search').'</a>] ';
 	}
 
-	public function ModulePage(){
+	public function ModulePage() {
 		$PIO = PIOPDO::getInstance();
 		$globalHTML = new globalHTML($this->board);
-
+	
 		$adminMode = isActiveStaffSession();
-
+	
 		$searchKeyword = isset($_GET['keyword']) ? trim($_GET['keyword']) : ''; // The text you want to search
 		$dat = '';
 		$globalHTML->head($dat);
-		$links = '[<a href="'.$this->config['PHP_SELF2'].'?'.time().'">'._T('return').'</a>]';
+		$links = '[<a href="' . $this->config['PHP_SELF2'] . '?' . time() . '">' . _T('return') . '</a>]';
 		$dat .= $links;
-		
+	
 		$dat .= '
-			<h2 class="theading2">'._T('search_top').'</h2>
-				<div class="modulePageContent">
-			';
-
-			$keyword = $_GET['keyword'] ?? '';
-			$field = $_GET['field'] ?? 'com';
-			$method = $_GET['method'] ?? 'AND';
-			
-			$dat .= '
-				<form action="'.$this->config['PHP_SELF'].'" method="get">
-					<input type="hidden" name="mode" value="module">
-					<input type="hidden" name="load" value="mod_search">
-			
-					<div id="search">
-						<ul>'._T('search_notice').'</ul>
-							
-						<table>
-							<tbody>
-								<tr>
-									<td class="postblock"><label for="searchKeyword">'._T('search_keyword').'</label></td>
-									<td><input type="text" class="inputtext" id="searchKeyword" name="keyword" value="'.htmlspecialchars($keyword, ENT_QUOTES).'"></td>
-								</tr>
-								<tr>
-									<td class="postblock"><label for="searchTarget">'._T('search_target').'</label></td>
-									<td>
-										<select id="searchTarget" name="field">
-											<option value="com"'.($field === 'com' ? ' selected="selected"' : '').'>'._T('search_target_comment').'</option>
-											<option value="name"'.($field === 'name' ? ' selected="selected"' : '').'>'._T('search_target_name').'</option>
-											<option value="sub"'.($field === 'sub' ? ' selected="selected"' : '').'>'._T('search_target_topic').'</option>
-											<option value="no"'.($field === 'no' ? ' selected="selected"' : '').'>'._T('search_target_number').'</option>
-										</select>
-									</td>
-								</tr>
-								<tr>
-									<td class="postblock"><label for="searchMethod">'._T('search_method').'</label></td>
-									<td>
-										<select id="searchMethod" name="method">
-											<option value="AND"'.($method === 'AND' ? ' selected="selected"' : '').'>'._T('search_method_and').'</option>
-											<option value="OR"'.($method === 'OR' ? ' selected="selected"' : '').'>'._T('search_method_or').'</option>
-										</select>
-									</td>
-								</tr>
-							</tbody>
-						</table>
-			
-						<div class="buttonSection">
-							<input type="submit" value="'._T('search_submit_btn').'">
-						</div>
+			<h2 class="theading2">' . _T('search_top') . '</h2>
+			<div class="modulePageContent">
+		';
+	
+		// Get the filters (search filters in this case)
+		$keyword = $_GET['keyword'] ?? '';
+		$field = $_GET['field'] ?? 'com';
+		$method = $_GET['method'] ?? 'AND';
+	
+		// Form with search parameters
+		$dat .= '
+			<form action="' . $this->config['PHP_SELF'] . '" method="get">
+				<input type="hidden" name="mode" value="module">
+				<input type="hidden" name="load" value="mod_search">
+				<div id="search">
+					<ul>' . _T('search_notice') . '</ul>
+					<table>
+						<tbody>
+							<tr>
+								<td class="postblock"><label for="searchKeyword">' . _T('search_keyword') . '</label></td>
+								<td><input type="text" class="inputtext" id="searchKeyword" name="keyword" value="' . htmlspecialchars($keyword, ENT_QUOTES) . '"></td>
+							</tr>
+							<tr>
+								<td class="postblock"><label for="searchTarget">' . _T('search_target') . '</label></td>
+								<td>
+									<select id="searchTarget" name="field">
+										<option value="com"' . ($field === 'com' ? ' selected="selected"' : '') . '>' . _T('search_target_comment') . '</option>
+										<option value="name"' . ($field === 'name' ? ' selected="selected"' : '') . '>' . _T('search_target_name') . '</option>
+										<option value="sub"' . ($field === 'sub' ? ' selected="selected"' : '') . '>' . _T('search_target_topic') . '</option>
+										<option value="no"' . ($field === 'no' ? ' selected="selected"' : '') . '>' . _T('search_target_number') . '</option>
+									</select>
+								</td>
+							</tr>
+							<tr>
+								<td class="postblock"><label for="searchMethod">' . _T('search_method') . '</label></td>
+								<td>
+									<select id="searchMethod" name="method">
+										<option value="AND"' . ($method === 'AND' ? ' selected="selected"' : '') . '>' . _T('search_method_and') . '</option>
+										<option value="OR"' . ($method === 'OR' ? ' selected="selected"' : '') . '>' . _T('search_method_or') . '</option>
+									</select>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+					<div class="buttonSection">
+						<input type="submit" value="' . _T('search_submit_btn') . '">
 					</div>
-				</form>
-				<hr>
-			';
-			
-
-		if($searchKeyword){
+				</div>
+			</form>
+			<hr>
+		';
+	
+		if ($searchKeyword) {
 			$searchPage = $_GET['page'] ?? 0;
 			$searchPostsPerPage = $this->config['ModuleSettings']['SEARCH_POSTS_PER_PAGE'];
 			$searchPostOffset = $searchPostsPerPage * $searchPage;
 			$quoteLinksFromBoard = getQuoteLinksFromBoard($this->board);
-
+	
 			$searchField = $_GET['field']; // Search target (no:number, name:name, sub:title, com:text)
 			$searchMethod = $_GET['method']; // Search method
 			$searchKeyword = preg_split('/(　| )+/', strtolower(trim($searchKeyword))); // Search text is cut with spaces
-			if ($searchMethod=='REG') $searchMethod = 'AND';
+			if ($searchMethod == 'REG') $searchMethod = 'AND';
 			$hitPosts = $PIO->searchPosts($this->board, $searchKeyword, $searchField, $searchMethod, $searchPostsPerPage, $searchPostOffset) ?? []; // Directly return the matching article content array
-
+	
 			$resultList = '';
-
+	
 			$templateValues = ['{$BOARD_THREAD_NAME}' => ''];
-
+	
 			$postRenderer = new postRenderer($this->board, $this->config, $globalHTML, $this->moduleEngine, $this->templateEngine, $quoteLinksFromBoard);
-			
-			foreach($hitPosts as $hitPost) {
+	
+			foreach ($hitPosts as $hitPost) {
 				$hitPostThread = $hitPost['thread'];
 				$hitPostData = $hitPost['post'];
-
+	
 				$hitThreadResno = $hitPostThread['post_op_number'];
-				//$hitThreadPosts = $hitPostThread['posts'];
-
-				$postFormExtra = '';
-				$warnBeKill = '';
-				$warnOld = '';
-				$warnHidePost = '';
-				$warnEndReply = '';
-
-				// render the post
+				$totalPostHits = $hitPost['total_posts'];
+	
+				// Render the post
 				$resultList .= $postRenderer->render($hitPostData,
-				 $templateValues,
-				 $hitThreadResno,
-				 false,
-				 [$hitPostData],
-				 $adminMode,
-				 $postFormExtra,
-				 $warnBeKill,
-				 $warnOld,
-				 $warnHidePost,
-				 $warnEndReply,
-				 0,
-				 true);
-				$resultList .= $this->templateEngine->ParseBlock('THREADSEPARATE', []); 
+					$templateValues,
+					$hitThreadResno,
+					false,
+					[$hitPostData],
+					$adminMode,
+					'',
+					'',
+					'',
+					'',
+					0,
+					true);
+				$resultList .= $this->templateEngine->ParseBlock('THREADSEPARATE', []);
 			}
-
+	
 			$dat .= '<div id="searchresult">';
 			$dat .= $resultList;
 			$dat .= '</div>';
+	
+			// Create the filters array for the URL (without pagination)
+			$filters = [
+				'keyword' => implode('+', $searchKeyword),
+				'field'   => $searchField,
+				'method'  => $searchMethod
+			];
 
-			// (WIP)$dat .= $globalHTML->drawPager($searchPostsPerPage, 1000, $this->mypage);
+			// Generate the base URL with the filters (no pagination yet)
+			$baseUrl = generateFilteredUrl($this->mypage, $filters);
+			
+			// Now, draw the pager (this will handle appending the page parameter automatically)
+			$dat .= $globalHTML->drawPager($searchPostsPerPage, $totalPostHits, $baseUrl);
 		}
-
+	
 		echo $dat;
 	}
+	
 }
