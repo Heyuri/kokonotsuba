d678542a880bf2d8f926c9598a026bcb659a0456
hachikuji <hachikujI@yuisuki.net>
2025-05-17 22:17:36 +1000

    PHP 8

--- code/action_log/actionClass.php
+++ code/action_log/actionClass.php
@@ -1,15 +1,21 @@
 <?php
 //encapsulate action from db
 class loggedActionEntry {
-	public $time_added, $name, $role, $log_action, $id, $ip_address, $board_uid, $board_title;
+	public readonly string $time_added;
+	public readonly string $name;
+	public readonly string $role;
+	public readonly string $log_action;
+	public readonly int $id;
+	public readonly string $ip_address;
+	public readonly int $board_uid;
+	public readonly string $board_title;
 
-	public function getTimeAdded() { return $this->time_added; }
-	public function getName() { return $this->name; }
-	public function getRole() { return $this->role; }
-	public function getLogAction() { return $this->log_action; }
-	public function getId() { return $this->id; }
-	public function getIpAddress() { return $this->ip_address; }
-	public function getBoardUID() { return $this->board_uid; }
-	public function getBoardTitle() { return $this->board_title; }
+	public function getTimeAdded(): string { return $this->time_added; }
+	public function getName(): string { return $this->name; }
+	public function getRole(): string { return $this->role; }
+	public function getLogAction(): string { return $this->log_action; }
+	public function getId(): int { return $this->id; }
+	public function getIpAddress(): string { return $this->ip_address; }
+	public function getBoardUID(): int { return $this->board_uid; }
+	public function getBoardTitle(): string { return $this->board_title; }
 }
-
