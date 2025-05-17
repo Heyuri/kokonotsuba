5228ffa61dd799171a7fe0bbf2b1d0500ffcd7ef
hachikuji <hachikujI@yuisuki.net>
2025-05-17 22:18:00 +1000

    remove cookies for filtering

--- code/routers/routes/actionLogRoute.php
+++ code/routers/routes/actionLogRoute.php
@@ -31,65 +31,27 @@ class actionLogRoute {
 
 		$actionLogHtml = '';
 		
-		$filterAction = $_POST['filterformsubmit'] ?? null;
-		
-		if ($_SERVER['REQUEST_METHOD'] === 'POST' && $filterAction === 'filter') {
-			$filterRoleFromPOST = $_POST['filterrole'] ?? '';
-			$filterBoardFromPOST = $_POST['filterboard'] ?? '';
-			
-			$filterIP = htmlspecialchars($_POST['filterip'] ?? '');
-			$filterDateBefore = htmlspecialchars($_POST['filterdatebefore'] ?? '');
-			$filterDateAfter = htmlspecialchars($_POST['filterdateafter'] ?? '');
-			$filterName = htmlspecialchars($_POST['filtername'] ?? '');
-			$filterBan = isset($_POST['bans']) ? 'checked' : '';
-			$filterDelete = isset($_POST['deleted']) ? 'checked' : '';
-			$filterRole = (is_array($filterRoleFromPOST) ? array_map('htmlspecialchars', $filterRoleFromPOST) : [htmlspecialchars($filterRoleFromPOST)]);
-			$filterBoard = (is_array($filterBoardFromPOST) ? array_map('htmlspecialchars', $filterBoardFromPOST) : [htmlspecialchars($filterBoardFromPOST)]);
-			
-			setcookie('filterip', $filterIP, time() + (86400 * 30), "/");
-			setcookie('filterdatebefore', $filterDateBefore, time() + (86400 * 30), "/");
-			setcookie('filterdateafter', $filterDateAfter, time() + (86400 * 30), "/");
-			setcookie('filtername', $filterName, time() + (86400 * 30), "/");
-			setcookie('filterban', $filterBan, time() + (86400 * 30), "/");
-			setcookie('filterdelete', $filterDelete, time() + (86400 * 30), "/");
-			setcookie('filterrole', json_encode($filterRole), time() + (86400 * 30), "/");
-			setcookie('filterboard', json_encode($filterBoard), time() + (86400 * 30), "/");
+		$defaultSelectedBoards = [$this->board->getBoardUID(), GLOBAL_BOARD_UID];
 
-			redirect($this->config['PHP_SELF'].'?mode=actionlog');
-		} else if($_SERVER['REQUEST_METHOD'] === 'POST' && $filterAction === 'filterclear') {
-			setcookie('filterip', "", time() - 3600, "/");
-			setcookie('filterdatebefore', "", time() - 3600, "/");
-			setcookie('filterdateafter', "", time() - 3600, "/");
-			setcookie('filtername', "", time() - 3600, "/");
-			setcookie('filterban', "", time() - 3600, "/");
-			setcookie('filterdelete', "", time() - 3600, "/");
-			setcookie('filterrole', "", time() - 3600, "/");
-			setcookie('filterboard', "", time() - 3600, "/");
-			
-			redirect($this->config['PHP_SELF'].'?mode=actionlog');
-		}
-		
-		$filtersBoards = (isset($_COOKIE['filterboard'])) ? json_decode($_COOKIE['filterboard'], true) : [$this->board->getBoardUID(), GLOBAL_BOARD_UID];
-		$filtersRoles = (isset($_COOKIE['filterrole'])) ? json_decode($_COOKIE['filterrole'], true) : array_values($this->config['roles']); 
-		
 		//filter list for the database
 		$filters = [
-			'ip_address' => $_COOKIE['filterip'] ?? null,
-			'name' => $_COOKIE['filtername'] ?? null,
-			'ban' => $_COOKIE['filterban'] ?? null,
-			'deleted' => $_COOKIE['filterdelete'] ?? null,
-			'role' => $filtersRoles ?? '',
-			'board' => $filtersBoards ?? '',
-			'date_before' => $_COOKIE['filterdatebefore'] ?? '',
-			'date_after' => $_COOKIE['filterdateafter'] ?? '',
+			'ip_address' => $_GET['ip_address'] ?? null,
+			'name' => $_GET['filterName'] ?? null,
+			'ban' => $_GET['ban'] ?? null,
+			'deleted' => $_GET['deleted'] ?? null,
+			'role' => $_GET['role'] ?? null,
+			'board' => $_GET['board'] ?? $defaultSelectedBoards,
+			'date_before' => $_GET['date_before'] ?? null,
+			'date_after' => $_GET['date_after'] ?? null,
 		];
+
 		$tableEntries = '';
 		$limit = $this->config['ACTIONLOG_MAX_PER_PAGE'];
 		$page = isset($_REQUEST['page']) ? (int)$_REQUEST['page'] : 0;
 		$page = ($page >= 0) ? $page : 1;
 		$offset = $page * $limit;
 		
-		$this->globalHTML->drawModFilterForm($actionLogHtml, $this->board);
+		$this->globalHTML->drawModFilterForm($actionLogHtml, $this->board, $filters);
 		
 		$entriesFromDatabase = $this->actionLogger->getSpecifiedLogEntries($limit, $offset, $filters);
 		$numberOfActionLogs = $this->actionLogger->getAmountOfLogEntries($filters);
@@ -121,8 +83,8 @@ class actionLogRoute {
 			}
 		}
 		
-		$actionLogHtml .= "<div id=\"reloadTable\" class=\"centerText\">[<a href=\"{$this->config['PHP_SELF']}?mode=actionLog\">Reload table</a>]</div>
-			<table class=\"postlists\" id=\"actionlogtable\">
+		$actionLogHtml .= "
+		<table class=\"postlists\" id=\"actionlogtable\">
 				<thead>
 					<tr>
 						<th>Board title</th>
@@ -142,7 +104,7 @@ class actionLogRoute {
 
 		$actionLogHtml .= $this->globalHTML->drawPager($limit, $numberOfActionLogs, $this->globalHTML->fullURL().$this->config['PHP_SELF'].'?mode=actionlog');
 	
-		$htmlOutput = $this->adminPageRenderer->ParsePage('GLOBAL_ADMIN_PAGE_CONTENT', ['{$PAGE_CONTENT' => $actionLogHtml], true);
+		$htmlOutput = $this->adminPageRenderer->ParsePage('GLOBAL_ADMIN_PAGE_CONTENT', ['{$PAGE_CONTENT}' => $actionLogHtml], true);
 
 		echo $htmlOutput;
 	}
