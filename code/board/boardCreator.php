3e6b262bfc3d9b4e7e3ed55c653266ff88180ef6
hachikuji <hachikujI@yuisuki.net>
2025-05-22 04:10:30 +1000

    convert to tabs and ensure boardListed is an binary (1 or 0) integer

--- code/board/boardCreator.php
+++ code/board/boardCreator.php
@@ -6,79 +6,81 @@
 
 
 class boardCreator {
-    private $globalHTML;
-    private $config;
-    private $boardIO;
-    private $boardPathCachingIO;
+	private $globalHTML;
+	private $config;
+	private $boardIO;
+	private $boardPathCachingIO;
 
-    public function __construct($globalHTML, $config, $boardIO, $boardPathCachingIO) {
-        $this->globalHTML = $globalHTML;
-        $this->config = $config;
-        $this->boardIO = $boardIO;
-        $this->boardPathCachingIO = $boardPathCachingIO;
-    }
+	public function __construct($globalHTML, $config, $boardIO, $boardPathCachingIO) {
+		$this->globalHTML = $globalHTML;
+		$this->config = $config;
+		$this->boardIO = $boardIO;
+		$this->boardPathCachingIO = $boardPathCachingIO;
+	}
 
-    public function createNewBoard(string $boardTitle, string $boardSubTitle, string $boardIdentifier, bool $boardListed, string $boardPath): board|null {
-        $fullBoardPath = $boardPath . $boardIdentifier . '/';
-        $mockConfig = getTemplateConfigArray();
-        $backendDirectory = getBackendDir();
-        $cdnDir = $this->config['CDN_DIR'] . $boardIdentifier . '/';
+	public function createNewBoard(string $boardTitle, string $boardSubTitle, string $boardIdentifier, bool $boardListed, string $boardPath): board|null {
+		$fullBoardPath = $boardPath . $boardIdentifier . '/';
+		$mockConfig = getTemplateConfigArray();
+		$backendDirectory = getBackendDir();
+		$cdnDir = $this->config['CDN_DIR'] . $boardIdentifier . '/';
 
-        $createdPaths = [];
+		$boardListed = $boardListed ? 1 : 0;
 
-        try {
-            // Create full board path
-            $createdPaths[] = $this->createDirectory($fullBoardPath);
+		$createdPaths = [];
 
-            // Create image storage paths
-            $imgDir = $this->config['USE_CDN'] ? $cdnDir . $mockConfig['IMG_DIR'] : $fullBoardPath . $mockConfig['IMG_DIR'];
-            $thumbDir = $this->config['USE_CDN'] ? $cdnDir . $mockConfig['THUMB_DIR'] : $fullBoardPath . $mockConfig['THUMB_DIR'];
-            $createdPaths[] = $this->createDirectory($imgDir);
-            $createdPaths[] = $this->createDirectory($thumbDir);
+		try {
+			// Create full board path
+			$createdPaths[] = $this->createDirectory($fullBoardPath);
 
-            // Create koko.php in the directory
-            $requireString = "\"$backendDirectory{$this->config['PHP_SELF']}\"";
-            $this->createFileAndWriteText($fullBoardPath, $mockConfig['PHP_SELF'], "<?php require_once {$requireString}; ?>");
+			// Create image storage paths
+			$imgDir = $this->config['USE_CDN'] ? $cdnDir . $mockConfig['IMG_DIR'] : $fullBoardPath . $mockConfig['IMG_DIR'];
+			$thumbDir = $this->config['USE_CDN'] ? $cdnDir . $mockConfig['THUMB_DIR'] : $fullBoardPath . $mockConfig['THUMB_DIR'];
+			$createdPaths[] = $this->createDirectory($imgDir);
+			$createdPaths[] = $this->createDirectory($thumbDir);
 
-            // Create board-storage directory
-            $boardStorageDirectoryName = 'storage-' . $this->boardIO->getNextBoardUID();
-            $dataDir = getBoardStoragesDir() . $boardStorageDirectoryName;
-            $createdPaths[] = $this->createDirectory($dataDir);
+			// Create koko.php in the directory
+			$requireString = "\"$backendDirectory{$this->config['PHP_SELF']}\"";
+			$this->createFileAndWriteText($fullBoardPath, $mockConfig['PHP_SELF'], "<?php require_once {$requireString}; ?>");
 
-            // Generate config file
-            $boardConfigName = generateNewBoardConfigFile();
+			// Create board-storage directory
+			$boardStorageDirectoryName = 'storage-' . $this->boardIO->getNextBoardUID();
+			$dataDir = getBoardStoragesDir() . $boardStorageDirectoryName;
+			$createdPaths[] = $this->createDirectory($dataDir);
 
-            // Add board to database
-            $this->boardIO->addNewBoard($boardIdentifier, $boardTitle, $boardSubTitle, $boardListed, $boardConfigName, $boardStorageDirectoryName);
+			// Generate config file
+			$boardConfigName = generateNewBoardConfigFile();
 
-            // Initialize and create boardUID.ini
-            $newBoardUID = $this->boardIO->getLastBoardUID();
-            $this->createFileAndWriteText($fullBoardPath, 'boardUID.ini', "board_uid = $newBoardUID");
+			// Add board to database
+			$this->boardIO->addNewBoard($boardIdentifier, $boardTitle, $boardSubTitle, $boardListed, $boardConfigName, $boardStorageDirectoryName);
 
-            // Add the board's physical path to the path cache table
-            $this->boardPathCachingIO->addNewCachedBoardPath($newBoardUID, $fullBoardPath);
+			// Initialize and create boardUID.ini
+			$newBoardUID = $this->boardIO->getLastBoardUID();
+			$this->createFileAndWriteText($fullBoardPath, 'boardUID.ini', "board_uid = $newBoardUID");
 
-            // Rebuild the new board
-            $newBoardFromDatabase = $this->boardIO->getBoardByUID($newBoardUID);
-            $newBoardFromDatabase->rebuildBoard();
+			// Add the board's physical path to the path cache table
+			$this->boardPathCachingIO->addNewCachedBoardPath($newBoardUID, $fullBoardPath);
 
-            return $newBoardFromDatabase;
-        } catch (Exception $e) {
-            $this->rollbackCreatedPaths($createdPaths);
-            $this->globalHTML->error($e->getMessage());
-        }
-        return null;
-    }
+			// Rebuild the new board
+			$newBoardFromDatabase = $this->boardIO->getBoardByUID($newBoardUID);
+			$newBoardFromDatabase->rebuildBoard();
 
-    private function createDirectory(string $path) {
-        return createDirectoryWithErrorHandle($path, $this->globalHTML);
-    }
+			return $newBoardFromDatabase;
+		} catch (Exception $e) {
+			$this->rollbackCreatedPaths($createdPaths);
+			$this->globalHTML->error($e->getMessage());
+		}
+		return null;
+	}
 
-    private function createFileAndWriteText(string $directory, string $fileName, string $content) {
-        createFileAndWriteText($directory, $fileName, $content);
-    }
+	private function createDirectory(string $path) {
+		return createDirectoryWithErrorHandle($path, $this->globalHTML);
+	}
 
-    private function rollbackCreatedPaths(array $createdPaths) {
-        rollbackCreatedPaths($createdPaths);
-    }
+	private function createFileAndWriteText(string $directory, string $fileName, string $content) {
+		createFileAndWriteText($directory, $fileName, $content);
+	}
+
+	private function rollbackCreatedPaths(array $createdPaths) {
+		rollbackCreatedPaths($createdPaths);
+	}
 }
