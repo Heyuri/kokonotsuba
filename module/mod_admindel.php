aba2c8029a2e77209bc0f7fe36bff200470e7036
hachikuji <hachikujI@yuisuki.net>
2025-05-24 00:32:17 +1000

    remove debug

--- module/mod_admindel.php
+++ module/mod_admindel.php
@@ -31,7 +31,7 @@ class mod_admindel extends moduleHelper {
 		
 		$postBoard = searchBoardArrayForBoard($this->moduleBoardList, $post['boardUID']);
 
-		$modfunc.= '<span class="adminFunctions adminDeleteFunction">[<a href="'.$this->mypage.'&action=del&post_uid='.$post['post_uid'].'&excimer=1" title="Delete">D</a>]</span>';
+		$modfunc.= '<span class="adminFunctions adminDeleteFunction">[<a href="'.$this->mypage.'&action=del&post_uid='.$post['post_uid'].'" title="Delete">D</a>]</span>';
 		if ($post['ext'] && $FileIO->imageExists($post['tim'].$post['ext'], $postBoard)) $modfunc.= '<span class="adminFunctions adminDeleteFileFunction">[<a href="'.$this->mypage.'&action=imgdel&post_uid='.$post['post_uid'].'" title="Delete file">DF</a>]</span>';
 		$modfunc.= '<span class="adminFunctions adminDeleteMuteFunction">[<a href="'.$this->mypage.'&action=delmute&post_uid='.$post['post_uid'].'" title="Delete and mute for '.$this->JANIMUTE_LENGTH.' minute'.($this->JANIMUTE_LENGTH == 1 ? "" : "s").'">DM</a>]</span>';
 	}
