4e462ce464f85653a53318fa84eaf412b4086e28
kaguy4 <kaguy4@protonmail.com>
2025-05-25 17:33:38 +0300

    Update board-template.php

--- global/board-configs/board-template.php
+++ global/board-configs/board-template.php
@@ -2,7 +2,10 @@
 require __DIR__.'/../globalBoardConfig.php';
 
 
-$config['TITLEIMG'] = '';
-
 $config['IMG_DIR'] = 'src/';
 $config['THUMB_DIR'] = 'src/';
+
+/* 
+* Board modules - you can adjust module settings specifically for this board by overwriting the value of the module to false or 0
+* e.g $config['ModuleList']['mod_showip'] = false; 
+*/
