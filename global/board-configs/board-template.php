68d48718afbbd84743733b9f0a484c3f9c27539c
hachikuji <hachikujI@yuisuki.net>
2025-05-26 01:59:47 +1000

    update template with comments and removing uneeded values

--- global/board-configs/board-template.php
+++ global/board-configs/board-template.php
@@ -1,8 +1,11 @@
 <?php
+/*
+* This file is for board-specific configurations.
+*/
+
 require __DIR__.'/../globalBoardConfig.php';
 
-
-$config['TITLEIMG'] = '';
-
-$config['IMG_DIR'] = 'src/';
-$config['THUMB_DIR'] = 'src/';
+/* 
+* Board modules - you can adjust module settings specifically for this board by overwriting the value of the module to false or 0
+* e.g $config['ModuleList']['mod_showip'] = false; 
+*/
