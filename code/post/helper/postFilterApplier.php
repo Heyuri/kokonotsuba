e4400e50b3652bea4efe5db36f98ac3dc308a0be
hachikuji <hachikujI@yuisuki.net>
2025-08-20 23:03:16 +1000

    pass ref_url to autoLink in postFilterApplier

--- code/post/helper/postFilterApplier.php
+++ code/post/helper/postFilterApplier.php
@@ -11,7 +11,7 @@ class postFilterApplier {
 
 	public function applyFilters(string &$com, string &$email): void {
 		if ($this->config['AUTO_LINK']) {
-			$com = autoLink($com);
+			$com = autoLink($com, $this->config['REF_URL']);
 		}
 		if ($this->config['FORTUNES'] && stristr($email, 'fortune')) {
 			$this->fortune->apply($com, $email);
