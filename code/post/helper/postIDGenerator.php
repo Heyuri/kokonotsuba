a15b8f74315fa642ea4a097977912788635a4d65
hachikuji <hachikujI@yuisuki.net>
2025-08-27 04:02:26 +1000

    generate the hash anyway regardless if displaying it is enabled

--- code/post/helper/postIDGenerator.php
+++ code/post/helper/postIDGenerator.php
@@ -16,26 +16,24 @@ class postIdGenerator {
 	public function generate(?string $email, int $time, int $threadNumber): string {
 		$roleLevel = $this->staffSession->getRoleLevel();
 
-        if ($this->config['DISP_ID']) { // ID display enabled
-			if ($roleLevel == \Kokonotsuba\Root\Constants\userRole::LEV_ADMIN && $this->config['DISP_ID'] == 2) {
-				return ' ID:ADMIN';
-			} elseif ($roleLevel == \Kokonotsuba\Root\Constants\userRole::LEV_MODERATOR && $this->config['DISP_ID'] == 2) {
-				return ' ID:MODERATOR';
-			} elseif (stristr($email, 'sage') && $this->config['DISP_ID'] == 2) {
-				return ' ID:Heaven';
-			} else {
-				$ip = new IPAddress;
-				$idSeed = $this->config['IDSEED'];
-				$postNo = $threadNumber ? $threadNumber : ($this->board->getLastPostNoFromBoard() + 1);
-				$baseString = $ip . $idSeed . $postNo;
+		if ($roleLevel == \Kokonotsuba\Root\Constants\userRole::LEV_ADMIN) {
+			return ' ADMIN';
+		} elseif ($roleLevel == \Kokonotsuba\Root\Constants\userRole::LEV_MODERATOR) {
+			return ' MODERATOR';
+		} elseif (stristr($email, 'sage')) {
+			return ' Heaven';
+		} else {
+			$ip = new IPAddress;
+			$idSeed = $this->config['IDSEED'];
+			$postNo = $threadNumber ? $threadNumber : ($this->board->getLastPostNoFromBoard() + 1);
+			$baseString = $ip . $idSeed . $postNo;
 
-				switch ($this->config['ID_MODE']) {
-					case 0:
-						return ' ID:' . substr(crypt(md5($baseString), 'id'), -8);
-					case 1:
-						$baseString .= gmdate('Ymd', $time + $this->config['TIME_ZONE'] * 60 * 60);
-						return ' ID:' . substr(crypt(md5($baseString), 'id'), -8);
-				}
+			switch ($this->config['ID_MODE']) {
+				case 0:
+					return substr(crypt(md5($baseString), 'id'), -8);
+				case 1:
+					$baseString .= gmdate('Ymd', $time + $this->config['TIME_ZONE'] * 60 * 60);
+					return substr(crypt(md5($baseString), 'id'), -8);
 			}
 		}
 		return '';
