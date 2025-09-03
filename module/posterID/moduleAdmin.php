cff1ddca304c725ddab80b91d5cef3edf352de84
hachikuji <hachikujI@yuisuki.net>
2025-09-03 16:47:01 +1000

    save checkbox selection

--- module/posterID/moduleAdmin.php
+++ module/posterID/moduleAdmin.php
@@ -26,12 +26,41 @@ class moduleAdmin extends abstractModuleAdmin {
 				$this->renderPostFormCheckbox($postFormAdminSection);
 			}
 		);
+
+		$this->moduleContext->moduleEngine->addRoleProtectedListener(
+			$this,
+			'RegistBegin',
+			function() {
+				$this->onRegistBegin();
+			}
+		);
 	}
 
 	private function renderPostFormCheckbox(string &$adminPostFormCheckbox): void {
+		// cookie for whether the checkbox is selected
+		$overidePostIdCookie = $_COOKIE['overidePostId'] ?? null;
+
+		// string for 'checked'
+		$checked = $overidePostIdCookie ? 'checked=""' : '';
+
+		// checkbox html
 		$adminPostFormCheckbox .= '
 			<span class="postFormAdminFunc overidePostId">
-				<label class="filterSelectBoardItem" title="If enabled, your post\'s hash will be generated normally"><input name="formModIdOveride" type="checkbox" value="on">Hide ID</label>
+				<label class="filterSelectBoardItem" title="If enabled, your post\'s hash will be generated normally"><input name="formModIdOveride" type="checkbox" value="on" ' . htmlspecialchars($checked) . '>Hide ID</label>
 			</span>';
+	}
+
+	private function onRegistBegin(): void {
+		// Hide ID checkbox from request
+		$formModIdOveride = !empty($_POST['formModIdOveride']) ?? null;
+
+		// if a post has been submitted with it selected, then set the cookie so its persistent
+		if($formModIdOveride) {
+			setcookie("overidePostId", "1", time() + 86400, "/");
+		}
+		// clear the cookie if it wasn't selected
+		else {
+			setcookie("overidePostId", "", time() - 3600, "/");
+		}
 	}
 }
