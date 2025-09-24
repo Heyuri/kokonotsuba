642b36091763032f8902b2435ee487bb4e1af08e
hachikuji <hachikujI@yuisuki.net>
2025-09-25 05:49:41 +1000

    stop deleted posts showing up on the search

--- code/post/postSearchRepository.php
+++ code/post/postSearchRepository.php
@@ -4,7 +4,8 @@ class postSearchRepository {
 	public function __construct(
         private DatabaseConnection $databaseConnection,
         private readonly string $postTable, 
-        private readonly string $threadTable
+        private readonly string $threadTable,
+        private readonly string $deletedPostsTable
     ) {}
 
 	public function fetchPostsByLike(string $field, string $boardUID, string $encoded, int $limit, int $offset): array {
@@ -14,10 +15,12 @@ class postSearchRepository {
 		];
 
 		$query = "
-			SELECT p.*, t.*
+			SELECT p.*, t.*, dp.open_flag, dp.file_only, dp.post_uid AS deleted_post_uid, dp.by_proxy, dp.id as deleted_post_id
 			FROM {$this->postTable} p
 			LEFT JOIN {$this->threadTable} t ON p.thread_uid = t.thread_uid
+			LEFT JOIN {$this->deletedPostsTable} dp ON p.post_uid = dp.post_uid  
 			WHERE p.$field REGEXP :phrase AND p.boardUID = :board_uid
+			AND (COALESCE(dp.open_flag, 0) = 0 OR COALESCE(dp.file_only, 0) = 1 OR COALESCE(dp.by_proxy, 0) = 1)
 			ORDER BY p.no DESC
 			LIMIT $limit OFFSET $offset
 		";
@@ -47,10 +50,12 @@ class postSearchRepository {
 		];
 
 		$query = "
-			SELECT p.*, t.*
+			SELECT p.*, t.*, dp.open_flag, dp.file_only, dp.post_uid AS deleted_post_uid, dp.by_proxy, dp.id as deleted_post_id
 			FROM {$this->postTable} p
 			LEFT JOIN {$this->threadTable} t ON p.thread_uid = t.thread_uid
-			WHERE MATCH(p.$field) AGAINST (:search IN BOOLEAN MODE)
+			LEFT JOIN {$this->deletedPostsTable} dp ON p.post_uid = dp.post_uid
+			WHERE MATCH(p.$field) AGAINST (:search IN BOOLEAN MODE) 
+			AND (COALESCE(dp.open_flag, 0) = 0 OR COALESCE(dp.file_only, 0) = 1 OR COALESCE(dp.by_proxy, 0) = 1)
 			AND p.boardUID = :board_uid
 			ORDER BY p.no DESC
 			LIMIT $limit OFFSET $offset
