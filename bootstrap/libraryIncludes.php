cdb223c4138fd04961a017f4844f8a12b0e048df
dead-lolis <nekonoah08@proton.me>
2026-02-12 01:40:16 +1100

    fix paths

--- bootstrap/libraryIncludes.php
+++ bootstrap/libraryIncludes.php
@@ -1,25 +1,25 @@
 <?php
 // require the puchiko libary include file
-require __DIR__ . getBackendDir() . 'code/Puchiko/includes.php';
+require getBackendDir() . 'code/Puchiko/includes.php';
 
 // require koko libraries (these have to be included manually)
-require __DIR__ . getBackendCodeDir() . 'libraries/lib_admin.php';
-require __DIR__ . getBackendCodeDir() . 'libraries/lib_attachment.php';
-require __DIR__ . getBackendCodeDir() . 'libraries/lib_common.php';
-require __DIR__ . getBackendCodeDir() . 'libraries/lib_compatible.php';
-require __DIR__ . getBackendCodeDir() . 'libraries/lib_database.php';
-require __DIR__ . getBackendCodeDir() . 'libraries/lib_errorhandler.php';
-require __DIR__ . getBackendCodeDir() . 'libraries/lib_external.php';
-require __DIR__ . getBackendCodeDir() . 'libraries/lib_file.php';
-require __DIR__ . getBackendCodeDir() . 'libraries/lib_filter.php';
-require __DIR__ . getBackendCodeDir() . 'libraries/lib_post.php';
-require __DIR__ . getBackendCodeDir() . 'libraries/lib_query.php';
-require __DIR__ . getBackendCodeDir() . 'libraries/lib_rebuild.php';
-require __DIR__ . getBackendCodeDir() . 'libraries/lib_template.php';
-require __DIR__ . getBackendCodeDir() . 'libraries/html/boardList.php';
-require __DIR__ . getBackendCodeDir() . 'libraries/html/filterForms.php';
-require __DIR__ . getBackendCodeDir() . 'libraries/html/formAndLayout.php';
-require __DIR__ . getBackendCodeDir() . 'libraries/html/helperHtmlFunctions.php';
-require __DIR__ . getBackendCodeDir() . 'libraries/html/miscPartials.php';
-require __DIR__ . getBackendCodeDir() . 'libraries/html/pagers.php';
-require __DIR__ . getBackendCodeDir() . 'libraries/html/postHtmlFunctions.php';
+require getBackendCodeDir() . 'libraries/lib_admin.php';
+require getBackendCodeDir() . 'libraries/lib_attachment.php';
+require getBackendCodeDir() . 'libraries/lib_common.php';
+require getBackendCodeDir() . 'libraries/lib_compatible.php';
+require getBackendCodeDir() . 'libraries/lib_database.php';
+require getBackendCodeDir() . 'libraries/lib_errorhandler.php';
+require getBackendCodeDir() . 'libraries/lib_external.php';
+require getBackendCodeDir() . 'libraries/lib_file.php';
+require getBackendCodeDir() . 'libraries/lib_filter.php';
+require getBackendCodeDir() . 'libraries/lib_post.php';
+require getBackendCodeDir() . 'libraries/lib_query.php';
+require getBackendCodeDir() . 'libraries/lib_rebuild.php';
+require getBackendCodeDir() . 'libraries/lib_template.php';
+require getBackendCodeDir() . 'libraries/html/boardList.php';
+require getBackendCodeDir() . 'libraries/html/filterForms.php';
+require getBackendCodeDir() . 'libraries/html/formAndLayout.php';
+require getBackendCodeDir() . 'libraries/html/helperHtmlFunctions.php';
+require getBackendCodeDir() . 'libraries/html/miscPartials.php';
+require getBackendCodeDir() . 'libraries/html/pagers.php';
+require getBackendCodeDir() . 'libraries/html/postHtmlFunctions.php';
