14b919d91e43e5b3db26d9ecbf6753c38a58ead9
hachikuji <lol>
2026-02-27 10:36:25 +1100

    add soudane table to depedencies for post search

--- code/Kokonotsuba/post/postSearchRepository.php
+++ code/Kokonotsuba/post/postSearchRepository.php
@@ -14,6 +14,7 @@ class postSearchRepository {
 		private readonly string $threadTable,
 		private readonly string $deletedPostsTable,
 		private readonly string $fileTable,
+		private readonly string $soudaneTable
 	) {}
 	
 	private function buildParamters(array $fields, array $boardUids): array {
