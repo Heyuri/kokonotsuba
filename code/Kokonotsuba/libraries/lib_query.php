fe3353c486908d9df9d84f78a19d8f6d4bc8a476
hachikuji <lol>
2026-02-28 01:44:56 +0000

    dont bother with the id

--- code/Kokonotsuba/libraries/lib_query.php
+++ code/Kokonotsuba/libraries/lib_query.php
@@ -206,7 +206,6 @@ function mergeMultiplePostRows(null|false|array $rows): false|array {
 		// soudane data
 		if ($row['votes_total_count'] !== null) {
 			$posts[$uid]['votes'] = [
-				'id' => $row['soudane_id'],
 				'total_score' => $row['votes_total_count'],
 				'yeah_count' => $row['votes_yeah_count'],
 				'nope_count' => $row['votes_nope_count']
