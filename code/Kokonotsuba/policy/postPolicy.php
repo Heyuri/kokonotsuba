36d0714cc2b3aa3826ad4dd5ce9854347627a111
hachikuji <lol>
2026-03-23 14:46:49 +0000

    post policy update

--- code/Kokonotsuba/policy/postPolicy.php
+++ code/Kokonotsuba/policy/postPolicy.php
@@ -3,34 +3,39 @@
 namespace Kokonotsuba\policy;
 
 class postPolicy extends policyBase {
-    public function authenticatePostDeletion(string $passwordHash, string $suppliedPassword): bool {
-        // if the user's role is authorized to delete a post (regardless if they have the correct password)
-        // then the user is authenticated for deleting the post - return true
-        if($this->modCanDeletePost()) {
-            return true;
-        }
+	public function canDeletePost(string $passwordHash, string $suppliedPassword): bool {
+		// if the user's role is authorized to delete a post (regardless if they have the correct password)
+		// then the user is authenticated for deleting the post - return true
+		if($this->canStaffDelete()) {
+			return true;
+		}
 
-        // verify if the password supplied by the user is the correct password (provided by the hash)
-        // then return true, as the user is authorized to delete the post
-        else if(password_verify($suppliedPassword, $passwordHash)) {
-            return true;
-        }
+		else if(empty($passwordHash)) {
+			// if there is no password hash for the post - then the post is not password protected, and nobody can delete it
+			return false;
+		}
 
-        // no authentication methods were tripped - failed, return false
-        else {
-            return false;
-        }
-    }
+		// verify if the password supplied by the user is the correct password (provided by the hash)
+		// then return true, as the user is authorized to delete the post
+		else if(password_verify($suppliedPassword, $passwordHash)) {
+			return true;
+		}
 
-    private function modCanDeletePost(): bool {
-        // minimum role required for deleting posts
-        $canDeletePost = $this->authLevels['CAN_DELETE_POST'];
+		// no authentication methods were tripped - failed, return false
+		else {
+			return false;
+		}
+	}
 
-        // get if the user's role is at least the required role
-        $isAuthorized = $this->roleLevel->isAtLeast($canDeletePost);
+	public function canStaffDelete(): bool {
+		// minimum role required for deleting posts
+		$canDeletePost = $this->authLevels['CAN_DELETE_POST'];
 
-        // return result
-        return $isAuthorized;
-    }
+		// get if the user's role is at least the required role
+		$isAuthorized = $this->roleLevel->isAtLeast($canDeletePost);
+
+		// return result
+		return $isAuthorized;
+	}
 
 }
