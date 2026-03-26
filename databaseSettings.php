1d1e8f4a36538635ca6e61c82a5a3a852e260e49
hachikuji <lol>
2026-03-26 10:53:05 +0000

    db settings

--- databaseSettings.php
+++ databaseSettings.php
@@ -11,24 +11,27 @@ return [
 	'DATABASE_NAME' => 'kokonotsuba', //the database the instance will be using
 
 	/* Tables used by various parts of kokonotsuba, do not change them after installing */
-	'POST_TABLE' => 'posts', //post table, contains all posts on an instance
-	'FILE_TABLE' => 'files', //file table, stores meta data on files  
+	'POST_TABLE' => 'posts', // post table, contains all posts on an instance
+	'FILE_TABLE' => 'files', // file table, stores meta data on files  
 	'QUOTE_LINK_TABLE' => 'quote_links',
-	'REPORT_TABLE' => 'reports', //report table used by report module
-	'BAN_TABLE' => 'bans', //ban table used by adminban module
-	'BOARD_TABLE' => 'boards', //board table for all boards active on an instance
-	'BOARD_PATH_CACHE_TABLE' => 'board_paths', //for caching board paths
-	'THREAD_CACHE_TABLE' => 'thread_cache', //for thread html caching
-	'POST_NUMBER_TABLE' => 'post_numbers', //used for futaba-like `No. XXXX` system
-	'ACCOUNT_TABLE' => 'accounts', //staff account table
-	'ACTIONLOG_TABLE' => 'actionlog', //user log
-	'THREAD_TABLE' => 'threads', //holds all threads
-	'THREAD_REDIRECT_TABLE' => 'redirects', //for thread redirecting
+	'REPORT_TABLE' => 'reports', // report table used by report module
+	'BAN_TABLE' => 'bans', // ban table used by adminban module
+	'BOARD_TABLE' => 'boards', // board table for all boards active on an instance
+	'BOARD_PATH_CACHE_TABLE' => 'board_paths', // for caching board paths
+	'THREAD_CACHE_TABLE' => 'thread_cache', // for thread html caching
+	'POST_NUMBER_TABLE' => 'post_numbers', // used for futaba-like `No. XXXX` system
+	'ACCOUNT_TABLE' => 'accounts', // staff account table
+	'ACTIONLOG_TABLE' => 'actionlog', // user log
+	'THREAD_TABLE' => 'threads', // holds all threads
+	'THREAD_REDIRECT_TABLE' => 'redirects', // for thread redirecting
 	'DELETED_POSTS_TABLE' => 'deleted_posts', // for storing meta-data on deleted posts
 	'CAPCODE_TABLE' => 'capcodes', // for storing capcode-related data
-	'SPAM_STRING_TABLE' => 'spam_string_rules',
+	'SPAM_STRING_TABLE' => 'spam_string_rules', // for storing anti-spam rules
 	'SOUDANE_TABLE'	=> 'soudane_votes', // for storing soudane-related vote data
 	'THREAD_THEMES_TABLE' => 'thread_themes', // for storing thread themes
 	'LAST_THREAD_SUBMISSIONS_TABLE' => 'last_thread_submissions', // for tracking last thread submission timestamps per board
 	'NOTE_TABLE' => 'notes', // for storing notes left by staff
+	'PRIVATE_MESSAGE_TABLE' => 'private_messages', // for storing private messages
+	'BANNER_TABLE' => 'banners', // for storing banner data
+	'BANNER_AD_TABLE' => 'banner_ads', // for storing banner ad data
 ];
