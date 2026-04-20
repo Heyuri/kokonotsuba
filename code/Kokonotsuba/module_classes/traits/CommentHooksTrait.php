2bdd57a11a7818e90f28764294cf9ff49c7fb213
hachikuji <lol>
2026-04-20 06:48:11 +0000

    fix autoLink breaking emotes/emojis

--- code/Kokonotsuba/module_classes/traits/CommentHooksTrait.php
+++ code/Kokonotsuba/module_classes/traits/CommentHooksTrait.php
@@ -5,11 +5,10 @@ namespace Kokonotsuba\module_classes\traits;
 use Kokonotsuba\module_classes\moduleEngine;
 
 use function Kokonotsuba\libraries\html\quote_unkfunc;
-use function Puchiko\strings\autoLink;
 
 /**
- * Shared comment-processing pipeline: dispatches PostComment hooks,
- * applies greentext quoting, and auto-links URLs.
+ * Shared comment-processing pipeline: dispatches PostComment hooks
+ * and applies greentext quoting.
  */
 trait CommentHooksTrait {
 	abstract protected function getModuleEngine(): moduleEngine;
@@ -17,7 +16,6 @@ trait CommentHooksTrait {
 	protected function applyCommentHooks(string $comment): string {
 		$this->getModuleEngine()->dispatch('PostComment', [&$comment]);
 		$comment = quote_unkfunc($comment);
-		$comment = autoLink($comment);
 		return $comment;
 	}
 }
