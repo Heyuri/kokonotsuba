cf1fbbb31dfeb9f24898ab253069d1ef78d65b5a
hachikuji-san <160812410+hachikuji-san@users.noreply.github.com>
2026-04-20 04:09:34 +0000

    Update globalconfig.php

--- global/globalconfig.php
+++ global/globalconfig.php
@@ -60,6 +60,7 @@ $config['AuthLevels']['CAN_DELETE_NOTE'] = Kokonotsuba\userRole::LEV_ADMIN;
 $config['AuthLevels']['CAN_EDIT_POST'] = Kokonotsuba\userRole::LEV_MODERATOR;
 $config['AuthLevels']['CAN_BAN_FILES'] = Kokonotsuba\userRole::LEV_MODERATOR;
 $config['AuthLevels']['CAN_MANAGE_PMS'] = Kokonotsuba\userRole::LEV_ADMIN;
+$concifg['AuthLevels']['CAN_MANAGE_ANTI_SPAM_SYSTEM'] = Kokonotsuba\userRole::LEV_MODERATOR;
 
 // Capcode formats (put '%s' where you want the original name)
 $config['JCAPCODE_FMT'] = '%s';
